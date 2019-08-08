44ad564faa5b3ad97fbf37b35b8ccb25063a1fd6
felsi13 <13felsi@gmail.com>
2019-08-08 14:39:53 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    tabs: [
-      { title: 'Tab 1', content: 'Some text 1' },
-      { title: 'Tab 2', content: 'Some text 2' },
-      { title: 'Tab 3', content: 'Some text 3' },
-    ],
+    tabs: [],
   };
 
   render() {
