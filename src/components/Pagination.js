aa50b0fd81cf72890db5ebc1fd66f3b3e93666b1
scandal0us <d.l.lutsak@gmail.com>
2019-08-13 15:23:03 +0300

    Solution

---
+++ src/components/Pagination.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+
+const Pagination = ({
+  total,
+  totalItems,
+  perPage = 5,
+  page = 1,
+  onPageChange,
+  withInfo,
+  showFrom,
+  showTo,
+  posts,
+}) => (
+  <>
+    <h1>Pagination component</h1>
+    <ul>
+      {posts.slice((perPage * (page - 1)), (perPage * page)).map(post =>
+        <li key={post.id}>
+          <span>{post.id} </span>
+          <span>{post.title}</span>
+        </li>
+      )}
+    </ul>
+    {withInfo ? <div>({showFrom} - {showTo} of {totalItems})</div> : ''}
+    <button
+      type="button"
+      disabled={page === 1}
+      onClick={() => onPageChange(page - 1)}
+    >
+      Prev
+    </button>
+    {(Array.from(Array(total), (v, k) => k + 1)).map(item =>
+      <button
+        type="button"
+        key={item}
+        className={classNames({
+          'active': item === page,
+        })}
+        onClick={() => onPageChange(item)}
+      >
+        {item}
+      </button>
+    )}
+    <button
+      type="button"
+      disabled={page === total}
+      onClick={() => onPageChange(page + 1)}
+    >
+      Next
+    </button>
+  </>
+);
+
+Pagination.propTypes = {
+  total: PropTypes.number,
+  totalItems: PropTypes.number,
+  perPage: PropTypes.number,
+  page: PropTypes.number,
+  onPageChange: PropTypes.func,
+  withInfo: PropTypes.bool,
+  showFrom: PropTypes.number,
+  showTo: PropTypes.number,
+  posts: PropTypes.array,
+};
+
+export default Pagination;
