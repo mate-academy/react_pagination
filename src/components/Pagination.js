ebb24422a643805c20b49522d2fcd68025809a22
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-24 18:27:12 +0300

    #6

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import shortid from 'shortid';
+
 import classNames from 'classnames';
 import './Pagination.scss';
 
 export default class Pagination extends Component {
-  handlerPageClick = pageNumber => this.props.onPageChange(pageNumber);
+  handlePageClick = pageNumber => this.props.onPageChange(pageNumber);
 
-  handlerPageKeyPress = pageNumber => this.handlerPageClick(pageNumber);
+  handlePageKeyPress = pageNumber => this.handlePageClick(pageNumber);
+
+  blockPageItem = (i) => {
+    const {
+      total, perPage, page, withInfo,
+    } = this.props;
+    const pagesQuantity = Math.ceil(total / perPage);
+
+    return (
+      <li
+        key={shortid.generate()}
+        className={classNames('page-item', {
+          active: i === page - 1,
+        })}
+        onClick={i !== page - 1
+          ? () => this.handlePageClick(i + 1)
+          : undefined}
+      >
+        <span className="page-link">
+          {i + 1}
+        </span>
+        {withInfo && (
+          <span className="page-info">
+            {`${i * perPage + 1} - ${
+              (i + 1) === pagesQuantity
+                ? total
+                : (i + 1) * perPage} of ${total}`}
+          </span>
+        )}
+      </li>
+    );
+  };
 
   render() {
     const {
-      total, perPage, page, withInfo, changedView,
+      total, perPage, page, changedView,
     } = this.props;
 
     const pagesQuantity = Math.ceil(total / perPage);
@@ -25,55 +58,36 @@ export default class Pagination extends Component {
               disabled: page === 1,
             })}
             onClick={page !== 1
-              ? () => this.handlerPageClick(page - 1)
+              ? () => this.handlePageClick(page - 1)
               : undefined}
             onKeyPress={page !== 1
-              ? () => this.handlerPageKeyPress(page - 1)
+              ? () => this.handlePageKeyPress(page - 1)
               : undefined}
           >
             <span className="page-link">Previous</span>
           </li>
-          {ischangedView && page !== 1 && (
-            <span className="page-dots">. . .</span>
-          )}
 
-          {pagesList.map((item, i) => (!ischangedView
-            || ((i === page - 1) || (i === page - 2) || (i === page))) && (
-            <li
-              key={`${item}-${i}`}
-              className={classNames('page-item', {
-                active: i === page - 1,
-              })}
-              onClick={i !== page - 1
-                ? () => this.handlerPageClick(i + 1)
-                : undefined}
-            >
-              <span className="page-link">
-                {i + 1}
-              </span>
-              {withInfo && (
-                <span className="page-info">
-                  {`${i * perPage + 1} - ${
-                    (i + 1) === pagesQuantity
-                      ? total
-                      : (i + 1) * perPage} of ${total}`}
-                </span>
-              )}
-            </li>
+          {pagesList.map((item, i) => (
+            (!ischangedView
+              || ((i === page - 1)
+                || (i === page - 2)
+                || (i === page)
+                || (i === 0)
+                || (i === pagesList.length - 1)))
+              ? this.blockPageItem(i)
+              : ((i === 1) || (i === pagesList.length - 2))
+              && (<span className="page-dots">. . .</span>)
           ))}
 
-          {ischangedView && page < pagesQuantity && (
-            <span className="page-dots">. . .</span>
-          )}
           <li
             className={classNames('page-item page-arrow', {
               disabled: page >= pagesQuantity,
             })}
             onClick={(page < pagesQuantity)
-              ? () => this.handlerPageClick(page + 1)
+              ? () => this.handlePageClick(page + 1)
               : undefined}
             onKeyPress={(page < pagesQuantity)
-              ? () => this.handlerPageKeyPress(page + 1)
+              ? () => this.handlePageKeyPress(page + 1)
               : undefined}
           >
             <span className="page-link">Next</span>
