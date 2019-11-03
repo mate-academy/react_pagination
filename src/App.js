1f00dd169eb15a9d86f0fdfeab6bf721779edb2a
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-03 15:04:01 +0300

    'Solution'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
+import PaginationWithContent from './components/Pagination/Pagination';
 
 class App extends React.Component {
-  state = {
-    tabs: [
-      { title: 'Tab 1', content: 'Some text 1' },
-      { title: 'Tab 2', content: 'Some text 2' },
-      { title: 'Tab 3', content: 'Some text 3' },
-    ],
-  };
-
   render() {
-    const { tabs } = this.state;
-
     return (
       <div className="App">
-        {/* eslint-disable-next-line */}
-        <h1>{tabs.length} tabs</h1>
+        <PaginationWithContent total={42} perPage={5} page={5} />
       </div>
     );
   }
