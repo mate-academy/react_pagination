8692c085867dd7c805d90dd283c7eb79f0cdd711
Ivan Beletskiy <beletsky02@gmail.com>
2020-10-30 22:05:48 +0200

    implement 1-4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
+import { Pagination } from './components/Pagination/Pagination';
 
-const App = () => <h1>Pagination</h1>;
+class App extends React.PureComponent {
+  state = {
+    currentPage: 1,
+  }
+
+  onPageChange = (page) => {
+    this.setState({
+      currentPage: page,
+    });
+  }
+
+  nextPage = () => {
+    this.setState(state => ({
+      currentPage: state.currentPage + 1,
+    }));
+  }
+
+  prevPage = () => {
+    this.setState(state => ({
+      currentPage: state.currentPage - 1,
+    }));
+  }
+
+  render() {
+    const { currentPage } = this.state;
+
+    return (
+      <Pagination
+        total={42}
+        perPage={5}
+        page={currentPage}
+        onPage={this.onPageChange}
+        onPrev={this.prevPage}
+        onNext={this.nextPage}
+        withInfo
+      />
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './Pagination.scss';
+
+function makeArray(minNumber, maxNumber) {
+  const arr = [];
+
+  for (let i = minNumber; i <= maxNumber; i += 1) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+export const Pagination = React.memo(({
+  total,
+  perPage,
+  page,
+  onPage,
+  onNext,
+  onPrev,
+  withInfo,
+}) => {
+  const numberOfPages = Math.ceil(total / perPage);
+  const pages = makeArray(1, numberOfPages);
+  const firstItem = (page - 1) * perPage + 1;
+  const lastItem = page * perPage > total ? total : page * perPage;
+
+  return (
+    <nav className="is-flex is-align-items-center m-6">
+      {withInfo && (
+        <p>
+          {`${firstItem} - ${lastItem} of ${total}`}
+        </p>
+      )}
+      <ul className="is-flex ml-3">
+        <li>
+          <button
+            type="button"
+            className="button"
+            onClick={onPrev}
+            disabled={page === 1}
+          >
+            Prev
+          </button>
+        </li>
+        {
+          pages.map(num => (
+            <li key={num}>
+              <button
+                type="button"
+                className={classNames({
+                  button: true,
+                  'is-info': num === page,
+                })}
+                onClick={() => onPage(num)}
+              >
+                {num}
+              </button>
+            </li>
+          ))
+        }
+        <li>
+          <button
+            type="button"
+            className="button"
+            onClick={onNext}
+            disabled={page === numberOfPages}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+});
+
+Pagination.propTypes = {
+  total: PropTypes.number.isRequired,
+  perPage: PropTypes.number,
+  page: PropTypes.number,
+  onPage: PropTypes.func.isRequired,
+  onPrev: PropTypes.func.isRequired,
+  onNext: PropTypes.func.isRequired,
+  withInfo: PropTypes.bool,
+};
+
+Pagination.defaultProps = {
+  perPage: 5,
+  page: 1,
+  withInfo: false,
+};
