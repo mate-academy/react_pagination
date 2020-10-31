2dea0b6990f157cf5b8aba383cd204f71210f00f
38063 <xeilen1112@gmail.com>
2020-11-01 00:51:26 +0200

    -solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,73 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import './App.css';
+import Pagination from './components/Pagination/Pagination';
 
-const App = () => <h1>Pagination</h1>;
+class App extends PureComponent {
+  state = {
+    currentPage: 1,
+    itemsPerPage: 5,
+    totalItems: 19,
+  }
+
+  changePage = (page) => {
+    if (page === 'Previous' || page === 'Next') {
+      this.setState(state => ({
+        currentPage: page === 'Previous'
+          ? state.currentPage - 1
+          : state.currentPage + 1,
+      }));
+
+      return;
+    }
+
+    this.setState({
+      currentPage: page,
+    });
+  }
+
+  changeItemsPerPage = (e) => {
+    const { value } = e.target;
+
+    this.setState({
+      currentPage: 1,
+      itemsPerPage: +value,
+    });
+  }
+
+  render() {
+    const { currentPage, itemsPerPage, totalItems } = this.state;
+    const firstItem = (currentPage - 1) * itemsPerPage + 1;
+    const lastItem = Math.min((currentPage * itemsPerPage), totalItems);
+    const info = `${firstItem} - ${lastItem} of ${totalItems}`;
+
+    return (
+      <div className="pl-3">
+        <h1>{`Current Page ${currentPage}`}</h1>
+
+        <label>
+          set number of items per page
+          <select
+            value={itemsPerPage}
+            onChange={this.changeItemsPerPage}
+            className="form-control"
+          >
+            <option value={3}>3</option>
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+          </select>
+        </label>
+
+        <Pagination
+          onPageChange={this.changePage}
+          currentPage={currentPage}
+          perPage={itemsPerPage}
+          totalItems={totalItems}
+          withInfo={info}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Pagination = ({
+  currentPage,
+  perPage,
+  totalItems,
+  onPageChange,
+  withInfo,
+}) => {
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  const isActive = page => (
+    currentPage === page ? 'btn btn-primary' : 'btn btn-outline-primary'
+  );
+
+  const generatedPages = [...Array(totalPages).keys()].map(i => i + 1);
+  const lastPage = generatedPages.length;
+
+  const isDisableNextButton = currentPage === generatedPages.length;
+  const isDisablePreviousButton = currentPage === 1;
+
+  return (
+    <nav aria-label="Page navigation example">
+      {withInfo && <p>{withInfo}</p>}
+      <ul className="pagination">
+        <li className="page-item">
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={() => onPageChange('Previous')}
+            disabled={isDisablePreviousButton}
+          >
+            Previous
+          </button>
+        </li>
+
+        <li className="page-item">
+          <button
+            type="button"
+            className={isActive(1)}
+            onClick={() => onPageChange(1)}
+          >
+            1
+          </button>
+        </li>
+
+        {currentPage > 3 && (
+          <li className="page-item">
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              disabled
+            >
+              ...
+            </button>
+          </li>
+        )}
+
+        {generatedPages.map((page) => {
+          if (page === currentPage
+            || page === currentPage + 1
+            || page === currentPage - 1) {
+            if (page === 1
+              || page === lastPage) {
+              return null;
+            }
+
+            return (
+              <li key={page} className="page-item">
+                <button
+                  type="button"
+                  className={isActive(page)}
+                  onClick={() => onPageChange(page)}
+                >
+                  {page}
+                </button>
+              </li>
+            );
+          }
+
+          return null;
+        })
+        }
+
+        {currentPage < lastPage - 2 && (
+          <li className="page-item">
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              disabled
+            >
+              ...
+            </button>
+          </li>
+        )}
+
+        {totalItems > perPage && (
+          <li className="page-item">
+            <button
+              type="button"
+              className={isActive(lastPage)}
+              onClick={() => onPageChange(lastPage)}
+            >
+              {lastPage}
+            </button>
+          </li>
+        )}
+
+        <li className="page-item">
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={() => onPageChange('Next')}
+            disabled={isDisableNextButton}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  perPage: PropTypes.number.isRequired,
+  totalItems: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  withInfo: PropTypes.string,
+};
+
+Pagination.defaultProps = {
+  withInfo: '',
+};
+
+export default React.memo(Pagination);
