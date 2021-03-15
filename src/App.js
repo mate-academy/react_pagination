43a0fa759ad9c175d5299e985aeaf7f3c78be992
chri911 <delightsome@bigmir.net>
2021-03-15 09:49:13 +0200

    change App component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import React from 'react';
 import { Pagination } from './Pagination';
 import './App.css';
 
-class App extends React.Component {
-  state= {
-
-  }
-
-  render() {
-    return (
-      <>
-        <h1>Pagination</h1>
-        <Pagination
-          total={42}
-          perPage={5}
-          page={1}
-          withInfo="Your advertisement could be here"
-        />
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <h1>Pagination</h1>
+    <Pagination
+      total={42}
+      perPage={5}
+      page={1}
+      withInfo="Your advertisement could be here"
+    />
+  </>
+);
 
 export default App;
