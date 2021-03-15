21f7b44c91a75013352cdcda04972713fbf4c63e
Artem Topolskyi <topolskyiav@gmail.com>
2021-03-15 21:51:29 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { Pagination } from './components/Pagination';
 import './App.css';
 
-const App = () => <h1>Pagination</h1>;
+class App extends React.Component {
+  state = {
+    currentPage: 1,
+  };
+
+  currentPageChangeHandler = (page) => {
+    this.setState({ currentPage: page });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pagination</h1>
+
+        <Pagination
+          total={12}
+          perPage={5}
+          currentPage={this.state.currentPage}
+          pageChanger={this.currentPageChangeHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
