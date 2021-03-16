6e18818bd1de96e5c0024ffa5cc589a56ec259f8
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-16 14:52:14 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,60 @@
 import React from 'react';
+import { Pagination } from './Pagination/Pagination';
 import './App.css';
 
-const App = () => <h1>Pagination</h1>;
+class App extends React.PureComponent {
+  state = {
+    selectedPage: 1,
+  }
+
+  onPageChange = (page) => {
+    this.setState({ selectedPage: page });
+  }
+
+  nextPageHandler = (pagesQuantity) => {
+    this.setState((prevState) => {
+      if (prevState.selectedPage === pagesQuantity) {
+        return 0;
+      }
+
+      return {
+        selectedPage: prevState.selectedPage + 1,
+      };
+    });
+  }
+
+  prevPageHandler = () => {
+    this.setState((prevState) => {
+      if (prevState.selectedPage === 1) {
+        return 0;
+      }
+
+      return {
+        selectedPage: prevState.selectedPage - 1,
+      };
+    });
+  }
+
+  render() {
+    const { selectedPage } = this.state;
+
+    return (
+      <>
+        <h1>Pagination</h1>
+        <nav aria-label="Page navigation example">
+          <Pagination
+            total={42}
+            perPage={5}
+            page={selectedPage}
+            handlerPage={this.onPageChange}
+            prevPageHandler={this.prevPageHandler}
+            nextPageHandler={this.nextPageHandler}
+            withInfo={6}
+          />
+        </nav>
+      </>
+    );
+  }
+}
 
 export default App;
