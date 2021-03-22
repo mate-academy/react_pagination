9e2cbdaad4f56ed1adb3586be1b228d0cb316d3e
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-03-22 20:33:59 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -3,41 +3,7 @@ import { Pagination } from './Pagination/Pagination';
 import './App.css';
 
 class App extends React.PureComponent {
-  state = {
-    selectedPage: 1,
-  }
-
-  onPageChange = (page) => {
-    this.setState({ selectedPage: page });
-  }
-
-  nextPageHandler = (pagesQuantity) => {
-    this.setState((prevState) => {
-      if (prevState.selectedPage === pagesQuantity) {
-        return 0;
-      }
-
-      return {
-        selectedPage: prevState.selectedPage + 1,
-      };
-    });
-  }
-
-  prevPageHandler = () => {
-    this.setState((prevState) => {
-      if (prevState.selectedPage === 1) {
-        return 0;
-      }
-
-      return {
-        selectedPage: prevState.selectedPage - 1,
-      };
-    });
-  }
-
   render() {
-    const { selectedPage } = this.state;
-
     return (
       <>
         <h1>Pagination</h1>
@@ -45,10 +11,6 @@ class App extends React.PureComponent {
           <Pagination
             total={42}
             perPage={5}
-            page={selectedPage}
-            handlerPage={this.onPageChange}
-            prevPageHandler={this.prevPageHandler}
-            nextPageHandler={this.nextPageHandler}
             withInfo
           />
         </nav>
--- src/Pagination/Pagination.jsx
+++ src/Pagination/Pagination.jsx
@@ -6,60 +6,88 @@ import { NextPage } from '../NextPage/NextPage';
 import { Content } from '../Content/Content';
 import './Pagination.css';
 
-export const Pagination = ({
-  total,
-  perPage,
-  page,
-  handlerPage,
-  prevPageHandler,
-  nextPageHandler,
-  withInfo,
-}) => {
-  const pagesQuantity = Math.ceil(total / perPage);
-  const pages = Array(pagesQuantity).fill(1);
+export class Pagination extends React.Component {
+  state = {
+    selectedPage: 1,
+  }
 
-  return (
-    <>
-      <ul className="pagination">
-        <PrevPage selectedPage={page} prevPageHandler={prevPageHandler} />
-        {pages.map((_, i) => (
-          <li
-            key={i + 1}
-            className={classNames(
-              'page-item',
-              { active: (i + 1) === page },
-            )}
-            onClick={(e) => {
-              e.preventDefault();
-              handlerPage(i + 1);
-            }}
-          >
-            <a
-              className="page-link"
-              href={(i + 1)}
+  onPageChange = (page) => {
+    this.setState({ selectedPage: page });
+  }
+
+  pageHandler = (event) => {
+    const { textContent } = event.target;
+    event.preventDefault();
+    this.onPageChange(+textContent);
+  }
+
+  nextPageHandler = (pagesQuantity) => {
+    this.setState((prevState) => {
+      if (prevState.selectedPage === pagesQuantity) {
+        return 0;
+      }
+
+      return {
+        selectedPage: prevState.selectedPage + 1,
+      };
+    });
+  }
+
+  prevPageHandler = () => {
+    this.setState((prevState) => {
+      if (prevState.selectedPage === 1) {
+        return 0;
+      }
+
+      return {
+        selectedPage: prevState.selectedPage - 1,
+      };
+    });
+  }
+
+  render() {
+    const { selectedPage } = this.state;
+    const { total, perPage, withInfo } = this.props;
+
+    const pagesQuantity = Math.ceil(total / perPage);
+    const pages = Array(pagesQuantity).fill(1);
+
+    return (  
+      <>
+        <ul className="pagination">
+          <PrevPage selectedPage={selectedPage} prevPageHandler={this.prevPageHandler} />
+          {pages.map((_, i) => (
+            <li
+              key={i + 1}
+              className={classNames(
+                'page-item',
+                { active: (i + 1) === selectedPage },
+              )}
+              onClick={this.pageHandler}
             >
-              {i + 1}
-            </a>
-          </li>
-        ))
-        }
-        <NextPage
-          pagesQuantity={pagesQuantity}
-          selectedPage={page}
-          nextPageHandler={nextPageHandler}
-        />
-      </ul>
-      {withInfo && <Content />}
-    </>
-  );
+              <a
+                className="page-link"
+                href={(i + 1)}
+              >
+                {i + 1}
+              </a>
+            </li>
+          ))
+          }
+          <NextPage
+            pagesQuantity={pagesQuantity}
+            selectedPage={selectedPage}
+            nextPageHandler={this.nextPageHandler}
+          />
+        </ul>
+        {withInfo && <Content />}
+      </>
+    )
+  }
 };
 
 Pagination.propTypes = {
   total: PropTypes.number.isRequired,
   perPage: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
-  handlerPage: PropTypes.func.isRequired,
-  prevPageHandler: PropTypes.func.isRequired,
-  nextPageHandler: PropTypes.func.isRequired,
   withInfo: PropTypes.bool.isRequired,
 };
