da416ca95391bf195249a61f26dcea281e41fde4
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-26 16:50:32 +0200

    pagination solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './App.css';
+import Pagination from './components/Pagination';
 
-const App = () => <h1>Pagination</h1>;
+const data = [
+  'Тут',
+  'розміщені',
+  'дані',
+  'з',
+  'різних',
+  'сторінок',
+  'даного',
+  'сайту',
+  '!',
+  ')',
+];
+
+const App = () => (
+  <>
+    <h1>Pagination</h1>
+    <Pagination
+      total={10}
+      page={1}
+      perPage={1}
+      data={data}
+    />
+  </>
+);
 
 export default App;
