92721856e6b4c9a63c57b34678420cc1f687a65f
Kostrica <romiorw@ukr.net>
2021-09-07 19:21:00 +0300

    Fixed code after reviev

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -21,26 +21,32 @@ const Pagination: React.FC<Props & typeof defaultProps> = ({
 }) => {
   const totalPages = Math.ceil(total / perPage);
   const startItem = perPage * (page - 1) + 1;
-  const endItem = perPage * page > total ? total : perPage * page;
+  const maxEndItem = perPage * page;
+  const endItem = maxEndItem > total
+    ? total
+    : maxEndItem;
+  const arrPages = Array.from(Array(totalPages).keys());
 
   return (
     <section>
 
       {withInfo && <p className="info">{`${startItem} - ${endItem} of ${total}`}</p>}
 
-      <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className={classNames('page-item', { disabled: page === 1 })}>
-            <a
-              className="page-link"
-              href="/"
-              onClick={event => handleClick(event, page - 1)}
-            >
-              Previous
-            </a>
-          </li>
+      <nav
+        aria-label="Page navigation example"
+        className="nav"
+      >
+        <button
+          type="button"
+          className="button"
+          disabled={page === 1}
+          onClick={event => handleClick(event, page - 1)}
+        >
+          Previous
+        </button>
 
-          {Array.from(Array(totalPages).keys()).map(key => (
+        <ul className="pagination">
+          {arrPages.map(key => (
             <li
               key={key}
               className={classNames('page-item', { active: page - 1 === key })}
@@ -54,17 +60,16 @@ const Pagination: React.FC<Props & typeof defaultProps> = ({
               </a>
             </li>
           ))}
-
-          <li className={classNames('page-item', { disabled: page === totalPages })}>
-            <a
-              className="page-link"
-              href="/"
-              onClick={event => handleClick(event, page + 1)}
-            >
-              Next
-            </a>
-          </li>
         </ul>
+
+        <button
+          type="button"
+          className="button"
+          disabled={page === totalPages}
+          onClick={event => handleClick(event, page + 1)}
+        >
+          Next
+        </button>
       </nav>
     </section>
   );
