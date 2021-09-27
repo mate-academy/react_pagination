54a48fa04376baaa9373fe268af0660042606e20
Vasyl Perepichai <vperepichay@gmail.com>
2021-09-27 09:03:46 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
 import React from 'react';
 import './App.css';
+import Pagination from './Component/Pagination';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+class App extends React.Component {
+  state = {
+    page: 1,
+    perPage: 5,
+  };
+
+  changePage = (page:number) => {
+    this.setState({
+      page,
+    });
+  };
+
+  changePerPage = (perPage: number) => {
+    this.setState({
+      perPage,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Pagination</h1>
+        <Pagination
+          total={42}
+          perPage={this.state.perPage}
+          page={this.state.page}
+          onPageChange={this.changePage}
+          withInfo
+          onPerPageChange={this.changePerPage}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ src/Component/Pagination.tsx
@@ -0,0 +1,124 @@
+import React from 'react';
+import './Pagination.scss';
+
+type Props = {
+  total: number;
+  perPage: number;
+  page: number;
+  onPageChange: (page: number) => void;
+  onPerPageChange: (perPage: number) => void;
+  withInfo: boolean;
+};
+
+const Pagination: React.FC<Props> = ({
+  total,
+  perPage = 5,
+  page = 1,
+  onPageChange,
+  onPerPageChange,
+  withInfo = false,
+}) => {
+  const pagesArray = [];
+
+  const handleMoveForward = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    if (page === pagesArray.length) {
+      return;
+    }
+
+    onPageChange(page + 1);
+  };
+
+  const handleMoveBack = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    if (page === 1) {
+      return;
+    }
+
+    onPageChange(page - 1);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>, item: number) => {
+    event.preventDefault();
+    if (page !== item) {
+      onPageChange(item);
+    }
+  };
+
+  const pages = Math.ceil(total / perPage);
+
+  for (let i = 1; i <= pages; i += 1) {
+    pagesArray.push(i);
+  }
+
+  return (
+    <>
+      <label htmlFor="perPageItems">
+        Choose items per page:
+        <select
+          value={perPage}
+          name="perPageItems"
+          id="perPageItems"
+          onChange={event => onPerPageChange(+event.currentTarget.value)}
+        >
+          <option value="3">3</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+        </select>
+      </label>
+      <nav aria-label="Page navigation example">
+        {withInfo
+        && (
+          <div className="pagination-info">
+            {(page - 1) * perPage + 1}
+            {' - '}
+            {page === pagesArray.length ? total : (page * perPage)}
+            {' of '}
+            {total}
+          </div>
+        )}
+        <ul className="pagination">
+          <li
+            className={page === 1 ? 'page-item hiddenBtn' : 'page-item'}
+          >
+            <a
+              className="page-link"
+              href="https://"
+              onClick={handleMoveBack}
+            >
+              Previous
+            </a>
+          </li>
+          {pagesArray.map(item => (
+            <li
+              className="page-item"
+              key={item}
+            >
+              <a
+                className={page === item ? 'page-link active' : 'page-link'}
+                onClick={(event) => handleClick(event, item)}
+                href="https://"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+          <li
+            className={page === pagesArray.length ? 'page-item hiddenBtn' : 'page-item'}
+          >
+            <a
+              className="page-link"
+              href="https://"
+              onClick={handleMoveForward}
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Pagination;
