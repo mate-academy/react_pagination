fe7c55269d55069195ac94db46c373f682f3c240
Olha Blokhina <olhablokhina@Olhas-MacBook-Pro.local>
2022-04-18 15:23:04 +0300

    solution_to_tests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,5 +1,7 @@
 // WRITE TESTS HERE
 
+const { Children } = require("react");
+
 
 describe('Page', () => {
   beforeEach(() => {
@@ -7,44 +9,43 @@ describe('Page', () => {
   });
 
   it('should contain 42 items', () => {
-    cy.get('[value="9"]').click()
-    cy.get('p').should('contain', '42 of 42')
+    cy.get('p').should('contain', 42)
   });
 
   it('should contain 1-5 items by default', () => {
-    cy.get('[value="1"]').click()
     cy.get('p').should('contain', 5)
     cy.get('p').should('contain', 1)
   });
 
   it ('should be highlighted the button of the first page by default', () => {
-    cy.get('[value="1"]').should('have.class', 'active')
-  });
+    cy.get('button').contains('1').should('have.class', 'active')
+  })
 
   it ('should display 3 page after click on the button [3]', () => {
     cy.get('[value="3"]').click()
-    cy.get('[value="3"]').should('have.class', 'is-active')
+    cy.get('[value="3"]').should('have.class', 'active')
     cy.get('p').should('contain', 15)
-  });
+  })
 
   it('should display the next page after click on the button [Next]', () => {
-    cy.get('[type="button"]').contains('Next')
+    cy.get('button').contains('Next')
       .click()
-    cy.get('[value="2"]').should('have.class', 'is-active')
+    cy.get('[value="2"]').should('have.class', 'active')
     cy.get('p').should('contain', 10)
-  });
+  })
 
   it('should select 10 items for displaying from the list', () => {
     cy.get('select').select('10')
     cy.get('p').should('contain', 10)
-  });
+  })
 
   it('should display the previous page after click on the button [Previous]', () => {
       cy.get('[value="7"]').click()
-      cy.get('[type="button"]').contains('Previous')
+      cy.get('button').contains('Previous')
         .click()
-      cy.get('[value="6"]').should('have.class', 'is-active')
+      cy.get('button').contains('6').should('have.class', 'active')
       cy.get('p').should('contain', 30)
-  });
-  });
+    })
   
+  });
+ 
