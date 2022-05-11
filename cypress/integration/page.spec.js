b386d8640de4946adadd65000eccc213e701c33a
Olha Blokhina <olhablokhina@Olhas-MacBook-Pro.local>
2022-05-11 21:10:50 +0300

    NewTests

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -2,11 +2,17 @@
 
 const { Children } = require("react");
 
-
 describe('Page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
+
+  const page = {
+    getPage(value) {
+      return cy.get('.pagination')
+               .contains(`${value}`);
+  }
+  };
 
   it('should contain 42 items', () => {
     cy.getByDataCy('info')
@@ -21,51 +27,52 @@ describe('Page', () => {
   });
 
   it ('should be highlighted the button of the first page by default', () => {
-    cy.get('.pagination')
-      .contains('1')
-      .should('have.class', 'active')
+    cy.get('.pagination').contains('1')
+      .should('have.class', 'active');
   });
 
   it ('should display 3 page after click on the button [3]', () => {
-    cy.get('.pagination')
-      .contains('3')
+    page.getPage('3')
       .click();
-    cy.get('.pagination')
-      .contains('3')
+    page.getPage('3')
       .should('have.class', 'active');
     cy.getByDataCy('info')
       .should('contain', 15);
   });
 
   it('should display the next page after click on the button [Next]', () => {
-    cy.get('.pagination')
-      .contains('Next')
+    page.getPage('Next')
       .click();
-    cy.get('.pagination')
-      .contains('2')
+    page.getPage('2')
       .should('have.class', 'active');
     cy.getByDataCy('info')
       .should('contain', 10);
-  })
+  });
 
   it('should select 10 items for displaying from the list', () => {
     cy.get('select')
       .select('10');
     cy.getByDataCy('info')
       .should('contain', 10);
-  })
+  });
 
   it('should display the previous page after click on the button [Previous]', () => {
-      cy.get('.pagination')
-        .contains('7')
-        .click();
-      cy.get('.pagination')
-        .contains('Previous')
-        .click();
-      cy.get('.pagination')
-        .contains('6')
-        .should('have.class', 'active');
-      cy.getByDataCy('info')
-        .should('contain', 30);
-    });
+    page.getPage('7')
+      .click();
+    page.getPage('Previous')
+      .click();
+    page.getPage('6')
+      .should('have.class', 'active');
+    cy.getByDataCy('info')
+      .should('contain', 30);
   });
+
+  it('Should be disable buttons if a move is not possible now', () => {
+    page.getPage('Previous')
+      .should('be.disabled');
+    page.getPage('9')
+      .click();
+    page.getPage('Next')
+      .should('be.disabled');
+  });
+});
