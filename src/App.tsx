5e7bc3dd4bee055bd72f1ecb5be4dc8d3bb81295
Yevgeniy Savchuk <savchukevgeniy1@gmail.com>
2022-05-24 12:01:58 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import { Pagination } from './Components/Pagination/Pagination';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+  const [total, setTotal] = useState(42);
+
+  return (
+    <div className="app">
+      <h1>Pagination</h1>
+      <Pagination
+        total={total}
+        perPage={perPage}
+        page={page}
+        onTotal={setTotal}
+        onPageChange={setPage}
+        onPerPageChange={setPerPage}
+      />
+    </div>
+  );
+};
 
 export default App;
