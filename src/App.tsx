3acc282b3d15b21289385fa50017aad2bd8e2c17
Daria Veretyak <dariaveretyak@gmail.com>
2022-06-11 21:43:36 +0300

    changed total

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Pagination } from './components/Pagination';
 const App: React.FC = () => {
   const perPages = [3, 5, 10, 20];
 
+  const total = 42;
+
   const [perPage, setPerPage] = useState(5);
 
   const handleChange = (number: number) => {
@@ -16,7 +18,7 @@ const App: React.FC = () => {
     <div className="App">
       <h1 className="App__title">Pagination</h1>
       <Pagination
-        total={42}
+        total={total}
         onPerPageChange={handleChange}
         perPage={perPage}
         perPages={perPages}
