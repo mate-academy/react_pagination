e261401bf7067226c5c1232dc012bacdcad8f282
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-06-26 11:51:35 +0300

    page_pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Pagination } from './Pagination';
 import './App.css';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>Pagination</h1>
+    <Pagination />
+  </>
+);
 
 export default App;
