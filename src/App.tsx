5e93fbe85c8c177aa0b250035aa38d9d4e00e310
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-06-28 17:03:13 +0300

    added a button

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ const App: React.FC = () => {
   const pageContent = [];
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const [selectedPage, setSelectedPage] = useState(1);
+  const [selectedItemsPerPage, setSelectedItemPerPage] = useState(5);
 
   for (let i = 1; i <= amount; i += 1) {
     pageContent.push(`Content item ${i}`);
   }
 
-  const lastItem = selectedPage * itemsPerPage;
-  const firstItem = lastItem - itemsPerPage;
+  const lastItem = selectedPage * selectedItemsPerPage;
+  const firstItem = lastItem - selectedItemsPerPage;
   const currentContent = pageContent.slice(firstItem, lastItem);
 
   return (
@@ -23,23 +24,35 @@ const App: React.FC = () => {
       <h1 className="h1 text-center">
         Pagination
       </h1>
-      <form>
+      <form
+        method="submit"
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSelectedItemPerPage(itemsPerPage);
+        }}
+      >
         <div>Set items per page</div>
         <input
           type="number"
           className="input"
+          value={itemsPerPage}
           min="2"
           onChange={(event) => {
             setItemsPerPage(+event.target.value);
           }}
         />
+        <button
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
       <div className="container text-center">
         <Content content={currentContent} />
       </div>
       <Pagination
         total={amount}
-        perPage={itemsPerPage}
+        perPage={selectedItemsPerPage}
         onSelect={setSelectedPage}
       />
     </div>
