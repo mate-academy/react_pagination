2b963641929c1bce35dc105a2bd0dad29f0b13b8
mira1zu <mira1zu733@gmail.com>
2022-07-16 13:34:23 +0200

    Basic task was made

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+import Pagination from './Pagination';
+
+const App: React.FC = () => {
+  const [itemsAmount] = useState(42);
+  const [perPage, setPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const updatePages = (newPage: number, newPerPage: number = perPage) => {
+    const pageAmount = Math.ceil(itemsAmount / newPerPage);
+
+    setCurrentPage(
+      Math.max(Math.min(pageAmount, newPage), 1),
+    );
+
+    setPerPage(newPerPage);
+  };
+
+  const handlePageChange = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const value = Number(event.currentTarget.value);
+
+    if (value === currentPage) {
+      return;
+    }
+
+    updatePages(value);
+  };
+
+  const handlePerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newPerPage = Number(event.target.value);
+
+    updatePages(currentPage, newPerPage);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <Pagination
+          total={itemsAmount}
+          perPage={perPage}
+          page={currentPage}
+          withInfo
+          onPageChange={handlePageChange}
+          onPerPageChange={handlePerPageChange}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
