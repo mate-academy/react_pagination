c390c68ac0378e747841d79feff9e3b24b5afea7
Pavlo <Pmoroz2004@gmail.com>
2022-07-19 13:05:22 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 
-const App: React.FC = () => <h1>Pagination</h1>;
+const App: React.FC = () => {
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [total, setTotal] = useState(42);
+  const [perPage, setPerPage] = useState(5);
+
+  const onPageChange = (num: number) => {
+    setSelectedPage(num);
+  };
+
+  const onSetTotal = (num: number) => {
+    setTotal(num);
+  };
+
+  const onPerPageChange = (num: number) => {
+    setPerPage(num);
+  };
+
+  return (
+    <nav>
+      <Pagination
+        total={total}
+        perPage={perPage}
+        page={selectedPage}
+        onPageChange={onPageChange}
+        onSetTotal={onSetTotal}
+        onPerPageChange={onPerPageChange}
+      />
+    </nav>
+  );
+};
 
 export default App;
