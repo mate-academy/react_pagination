1e1a57ab3dda2fe04929aa7040eab5228a3c6f50
mhrynko <glukym@gmail.com>
2022-07-20 16:24:47 +0300

    Updates

--- static/js/main.48826ca7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreact_pagination=this.webpackJsonpreact_pagination||[]).push([[0],{3:function(n,t,e){n.exports=e(9)},8:function(n,t,e){},9:function(n,t,e){"use strict";e.r(t);var a=e(0),i=e.n(a),o=e(2),r=e.n(o),c=(e(8),function(){return i.a.createElement("h1",null,"Pagination")});r.a.render(i.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.48826ca7.chunk.js.map
