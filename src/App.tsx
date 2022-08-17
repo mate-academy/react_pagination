e747cf8334437f180e02a51b542941d5998d075f
Olha Stakhova <olha.stakhova@gmail.com>
2022-08-17 17:44:56 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 import { getNumbers } from './utils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,12 +8,29 @@ const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [total] = useState(items.length);
+  const [perPage, setPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const copyItems = [...items];
+  const onPageChange = (pageNumber: number) => {
+    return (
+      copyItems
+        .slice((pageNumber - 1) * perPage, (pageNumber - 1) * perPage + perPage)
+    );
+  };
+
+  const itemsOnPage = onPageChange(currentPage);
+  const firstNumberItemONpage = (itemsOnPage[0].split(' '))[1];
+  const lastNumberItemONpage = (itemsOnPage[itemsOnPage.length - 1]
+    .split(' '))[1];
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${currentPage} (items ${firstNumberItemONpage} - ${lastNumberItemONpage} of ${total})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +39,11 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPage}
+            onChange={(e) => {
+              setPerPage(Number(e.target.value));
+              setCurrentPage(1);
+            }}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,63 +57,14 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination
+        total={total}
+        perPage={perPage}
+        currentPage={currentPage}
+        itemsOnPage={itemsOnPage}
+        setCurrentPage={setCurrentPage}
+      />
+
     </div>
   );
 };
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,88 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+
+interface Props {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  itemsOnPage:string[],
+  setCurrentPage:(page : number) => void,
+}
+
+export const Pagination: React.FC<Props> = (props) => {
+  const {
+    total, perPage, currentPage, itemsOnPage, setCurrentPage,
+  } = props;
+
+  const totalPage = (Math.ceil(total / perPage));
+  const pagesNumbers = Array.from({ length: totalPage }, (_, i) => i + 1);
+
+  const handelPrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handelNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={cn('page-item', { disabled: currentPage === 1 })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={handelPrevPage}
+          >
+            «
+          </a>
+        </li>
+
+        {pagesNumbers.map(pageNumber => (
+          <li
+            className={cn(
+              'page-item',
+              { active: pageNumber === currentPage },
+            )}
+            key={pageNumber}
+          >
+            <a
+              data-cy="pageLink"
+              className={cn(
+                'page-link',
+              )}
+              href={`#${pageNumber - 1}`}
+              onClick={() => setCurrentPage(pageNumber)}
+            >
+              {pageNumber}
+            </a>
+          </li>
+
+        ))}
+
+        <li
+          className={cn(
+            'page-item',
+            { disabled: currentPage === pagesNumbers.length },
+          )}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === pagesNumbers.length}
+            onClick={handelNextPage}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {itemsOnPage.map(item => <li data-cy="item" key={item}>{item}</li>)}
+      </ul>
+    </>
+  );
+};
