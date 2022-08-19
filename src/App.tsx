036638a4e39a4c995d7303fa1e44340f2e3bbdfd
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-08-19 17:34:00 +0300

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 import { getNumbers } from './utils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,12 +8,23 @@ const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [selected, setSelected] = useState(1);
+  const [perPageSelector, setPerPageselector] = useState('5');
+
+  const pageBottom = (selected - 1) * +perPageSelector;
+  const pageTop = selected * +perPageSelector;
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${selected} (items ${pageBottom + 1}
+          ${' - '}
+          ${pageTop <= items.length ? pageTop : items.length})
+          ${' of '}
+          ${items.length}
+          `}
       </p>
 
       <div className="form-group row">
@@ -21,6 +33,11 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPageSelector}
+            onChange={(event) => {
+              setPerPageselector(event.target.value);
+              setSelected(1);
+            }}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -29,67 +46,32 @@ export const App: React.FC = () => {
           </select>
         </div>
 
-        <label htmlFor="perPageSelector" className="col-form-label col">
+        <label
+          htmlFor="perPageSelector"
+          className="col-form-label col"
+        >
           items per page
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        items={items}
+        perPageSelector={+perPageSelector}
+        selected={selected}
+        setSelected={setSelected}
+      />
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {items.filter((_, index) => {
+          return index >= pageBottom
+                  && index < pageTop;
+        }).map(item => (
+          <li
+            data-cy="item"
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,76 @@
-export const Pagination = () => {};
+import React from 'react';
+
+type Props = {
+  items: string[],
+  perPageSelector: number,
+  selected: number,
+  setSelected: React.Dispatch<React.SetStateAction<number>>
+};
+
+export const Pagination: React.FC<Props> = ({
+  items,
+  perPageSelector,
+  selected,
+  setSelected,
+}) => {
+  const numberPages = Math.ceil(items.length / +perPageSelector);
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={`page-item ${selected === 1 && 'disabled'}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+            onClick={() => {
+              if (selected === 1) {
+                return;
+              }
+
+              setSelected(prev => prev - 1);
+            }}
+          >
+            «
+          </a>
+        </li>
+        {items.filter((_, index) => index < numberPages)
+          .map((item, index) => (
+            <li
+              className={`page-item ${selected === index + 1 && 'active'}`}
+              key={item}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`${index + 1}`}
+                onClick={() => {
+                  setSelected(index + 1);
+                }}
+              >
+                {index + 1}
+              </a>
+            </li>
+          ))}
+        <li className={`page-item ${selected === numberPages && 'disabled'}`}>
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+            onClick={() => {
+              if (selected === numberPages) {
+                return;
+              }
+
+              setSelected(prev => prev + 1);
+            }}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
