cd5383abe1c16f82928e8509d6bec40d57fe267b
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-05 22:28:56 +0300

    Added Router wrapper component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,16 @@
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
