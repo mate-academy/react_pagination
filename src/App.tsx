a90d2398fafc9f7e99626737f09e0260bad81abe
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-09-16 00:04:43 +0300

    Some changes in markup

--- src/App.tsx
+++ src/App.tsx
@@ -41,26 +41,13 @@ export const App: React.FC = () => {
               setCurrentPage(1);
             }}
           >
-            <option
-              value="3"
-            >
-              3
-            </option>
-            <option
-              value="5"
-            >
-              5
-            </option>
-            <option
-              value="10"
-            >
-              10
-            </option>
-            <option
-              value="20"
-            >
-              20
-            </option>
+            <option value="3">3</option>
+
+            <option value="5">5</option>
+
+            <option value="10">10</option>
+
+            <option value="20">20</option>
           </select>
         </div>
 
