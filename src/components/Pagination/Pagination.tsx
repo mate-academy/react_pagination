c3004d2ab4969de87f60c240b914c83dd2d824c6
Max Bovtun <maxbovtun@gmail.com>
2022-10-10 21:07:57 +0300

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,89 @@
-export const Pagination = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+type Props = {
+  perPage: number,
+  allItems: string[],
+  onPageChange: (event: React.MouseEvent<HTMLElement>) => void,
+  paginationButtons: number[],
+  selectedPage: number,
+};
+
+export const Pagination: React.FC<Props> = ({
+  perPage,
+  allItems,
+  onPageChange,
+  paginationButtons,
+  selectedPage,
+}) => {
+  const onThisPage = allItems
+    .filter((_, i) => (
+      i >= (selectedPage * perPage - perPage)
+      && i < (selectedPage * perPage)
+    ));
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={classNames('page-item', {
+            disabled: selectedPage === 1,
+          })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={selectedPage === 1}
+            onClick={onPageChange}
+          >
+            «
+          </a>
+        </li>
+
+        {paginationButtons.map(button => {
+          return (
+            <li
+              key={button}
+              className={classNames('page-item', {
+                active: button === selectedPage,
+              })}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`#${button}`}
+                onClick={onPageChange}
+              >
+                {button}
+              </a>
+            </li>
+          );
+        })}
+
+        <li
+          className={classNames('page-item', {
+            disabled: selectedPage === paginationButtons.length,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={selectedPage === paginationButtons.length}
+            onClick={onPageChange}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {allItems.map((item) => {
+          return onThisPage.includes(item) && (
+            <li key={item} data-cy="item">{item}</li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
