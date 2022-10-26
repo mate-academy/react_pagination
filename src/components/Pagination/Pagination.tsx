08a22d532d45afc7619290cc60ba9698fd63f60a
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-10-26 12:22:15 +0300

    Implemented the paginator

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,91 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import React from 'react';
+import { uuid } from 'uuidv4';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const tabsCount = Math.ceil(total / perPage);
+  const tabs = Array(tabsCount).fill(1).map((_, i) => ({
+    number: i + 1,
+    id: uuid(),
+  }));
+
+  const getNextPage = () => {
+    if (currentPage !== tabsCount) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const getPrevPage = () => {
+    if (currentPage !== 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={cn(
+        'page-item',
+        { disabled: currentPage === 1 },
+      )}
+
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={getPrevPage}
+        >
+          «
+        </a>
+      </li>
+
+      {tabs.map(tab => (
+        <li
+          className={cn(
+            'page-item',
+            { active: currentPage === tab.number },
+          )}
+          key={tab.id}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${tab.number}`}
+            onClick={() => onPageChange(tab.number)}
+          >
+            {tab.number}
+          </a>
+        </li>
+      ))}
+
+      <li className={cn(
+        'page-item',
+        { disabled: currentPage === tabsCount },
+      )}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === tabsCount}
+          onClick={getNextPage}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
