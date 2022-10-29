9a14453b01f0b27359bbdf0d7d9890f1d58fd026
graift-off <graf.off.it@gmail.com>
2022-10-30 00:08:26 +0300

    feat: components were decomposed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import { List } from './components/List';
 import { Pagination } from './components/Pagination';
+import { PerPageSelect } from './components/PerPageSelect';
 import { getNumbers } from './utils';
 
-const items = getNumbers(1, 42)
+const items: Item[] = getNumbers(1, 42)
   .map(n => ({ title: `Item ${n}`, id: uuidv4() }));
 
 export const App: React.FC = () => {
@@ -36,27 +38,10 @@ export const App: React.FC = () => {
         {`Page ${currentPage} (items ${startIndex + 1} - ${endIndex} of ${items.length})`}
       </p>
 
-      <div className="form-group row">
-        {/* Move to separete component */}
-        <div className="col-3 col-sm-2 col-xl-1">
-          <select
-            data-cy="perPageSelector"
-            id="perPageSelector"
-            className="form-control"
-            value={perPage}
-            onChange={handlePerPageSelect}
-          >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-          </select>
-        </div>
-
-        <label htmlFor="perPageSelector" className="col-form-label col">
-          items per page
-        </label>
-      </div>
+      <PerPageSelect
+        perPage={perPage}
+        handlePerPageSelect={handlePerPageSelect}
+      />
 
       <Pagination
         total={items.length}
@@ -65,12 +50,7 @@ export const App: React.FC = () => {
         onPageChange={onPageChange}
       />
 
-      {/* Move to separate component */}
-      <ul>
-        {visibleItems.map(({ title, id }) => (
-          <li key={id} data-cy="item">{title}</li>
-        ))}
-      </ul>
+      <List items={visibleItems} />
     </div>
   );
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+interface Item {
+  title: string;
+  id: string;
+}
