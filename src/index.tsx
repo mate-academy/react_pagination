387bdbb731e7692eef3be9bf404dfc814344d730
Andriy Nosov <nosovandriy@gmail.com>
2022-11-17 20:40:37 +0200

    Add searchParams

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,3 @@
-// import ReactDOM from 'react-dom';
-// import { App } from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter,
