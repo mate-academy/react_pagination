9683949643c6214df70a2436e050c0987492ef7d
Denys Topchyi <miliarder97@gmail.com>
2022-11-28 14:55:33 +0200

    react_pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,83 @@
-export const Pagination = () => {};
+import classname from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: string[],
+  perPage: number,
+  currentPage: number,
+  onPageChange: (item: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total, perPage, onPageChange, currentPage,
+}) => {
+  const numberOfLinks = Math.ceil(42 / perPage);
+  const pageNumber = getNumbers(1, numberOfLinks);
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={classname({
+          'page-item': true,
+          disabled: currentPage === 1,
+        })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={() => onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+        {pageNumber.map(item => (
+          <li
+            key={item}
+            className={classname({
+              'page-item': true,
+              active: item === currentPage,
+            })}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${item}`}
+              onClick={() => onPageChange(item)}
+            >
+              {item}
+            </a>
+          </li>
+        ))}
+
+        <li className={classname({
+          'page-item': true,
+          disabled: currentPage === numberOfLinks,
+        })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === numberOfLinks}
+            onClick={() => onPageChange(currentPage + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        {total.map(item => (
+          <li
+            data-cy="item"
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
