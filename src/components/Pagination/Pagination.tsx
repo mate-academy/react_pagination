771da05a667525d38c179d6dd089e75b4d9e51af
OlhaArama <arama_o_f@ukr.net>
2022-12-12 20:28:19 +0200

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,102 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pages = [];
+  const numberOfPages = Math.ceil(total / perPage);
+
+  for (let i = 1; i <= numberOfPages; i += 1) {
+    pages.push(i);
+  }
+
+  const handlePrevPage = () => {
+    if (currentPage !== 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage !== numberOfPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={
+          classNames(
+            'page-item',
+            {
+              disabled: currentPage === 1,
+            },
+          )
+        }
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={handlePrevPage}
+          >
+            «
+          </a>
+        </li>
+        {pages.map(page => (
+          <li
+            key={page}
+            className={
+              classNames(
+                'page-item',
+                {
+                  active: page === currentPage,
+                },
+              )
+            }
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${page}`}
+              onClick={() => onPageChange(page)}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+        <li
+          className={
+            classNames(
+              'page-item',
+              {
+                disabled: currentPage === numberOfPages,
+              },
+            )
+          }
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === numberOfPages}
+            onClick={handleNextPage}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
