eb79940ef033591e656c59752831e6524e8a8e5d
Artem Hirzhev <a.hirzhev@gmail.com>
2023-01-08 17:38:30 +0200

    Initial commit

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,17 @@ export function getNumbers(from: number, to: number): number[] {
 
   return numbers;
 }
+
+export function getPages(perPage: number, total: number): number {
+  let result = 0;
+
+  if (total % perPage === 0) {
+    result = total / perPage;
+  }
+
+  if (total % perPage !== 0) {
+    result = Math.floor(total / perPage + 1);
+  }
+
+  return result;
+}
