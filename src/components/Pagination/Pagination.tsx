ec18be77cacef7b28fe05c4346c2e8703f97345f
metin <metinbicaksiz@gmail.com>
2023-01-10 22:43:10 +0300

    pagination done

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,72 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+interface Props {
+  page: number;
+  paginate: (num: number) => void;
+  totalPages: number;
+}
+
+export const Pagination: React.FC<Props> = ({
+  page,
+  paginate,
+  totalPages,
+}) => {
+  const pages = [];
+
+  for (let i = 1; i <= totalPages; i += 1) {
+    pages.push(i);
+  }
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item', {
+        disabled: page <= 1,
+      })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href={`#${page}`}
+          aria-disabled={page > 1 ? 'false' : 'true'}
+          onClick={() => page > 1 && paginate(page - 1)}
+        >
+          «
+        </a>
+      </li>
+      {
+        pages.map((num) => (
+          <li
+            className={cn('page-item', {
+              active: page === num,
+            })}
+            key={num}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${num}`}
+              onClick={() => paginate(num)}
+            >
+              {num}
+            </a>
+          </li>
+        ))
+      }
+      <li className={cn('page-item', {
+        disabled: page >= pages.length,
+      })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href={`#${page}`}
+          aria-disabled={page === totalPages ? 'true' : 'false'}
+          onClick={() => page !== totalPages && paginate(page + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
