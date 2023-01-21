7b894f87dbd358d7a2e5c8cad06df2a5984766fc
Anatolii <batstolya@gmail.com>
2023-01-22 01:43:16 +0200

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,86 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const countPageItem = Math.ceil(total / perPage);
+
+  const pageItem = new Array(countPageItem).fill(1).map((_, i) => i + 1);
+
+  const handlerPage = (pageNum:number) => {
+    onPageChange(pageNum);
+  };
+
+  return (
+    <ul className="pagination">
+      <li
+        className={classNames('page-item', {
+          disabled: currentPage === 1,
+        })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1 && 'true'}
+          onClick={() => {
+            onPageChange(currentPage > 1
+              ? currentPage - 1
+              : currentPage);
+          }}
+        >
+          «
+        </a>
+      </li>
+      {pageItem.map((page) => (
+        <li
+          className={classNames('page-item', {
+            active: currentPage === page,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => {
+              handlerPage(page);
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li
+        className={classNames('page-item', {
+          disabled: currentPage === countPageItem,
+        })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === pageItem.length && 'true'}
+          onClick={() => {
+            onPageChange(
+              currentPage < countPageItem
+                ? currentPage + 1
+                : currentPage,
+            );
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
