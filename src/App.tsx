d6de4227060f7f3e0cd256503a977a604ecea3b4
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-01-25 07:54:54 -0600

    updated Pagination component and implemented itemsPerPage state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 import { getNumbers } from './utils';
+
+//! Required Props on <Pagination /> :
+// * <Pagination
+// * total={42} // total number of items to paginate
+// * perPage={5} // number of items per page
+// * currentPage={1} // optional with 1 by default
+// * onPageChange={(page) => { ... }}
+// * / >
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+
+  const total = items.length;
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page 1 (items 1 - 5 of ${total})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +34,8 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={itemsPerPage}
+            onChange={(event) => setItemsPerPage(Number(event.target.value))}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -33,64 +48,7 @@ export const App: React.FC = () => {
           items per page
         </label>
       </div>
-
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination />
     </div>
   );
 };
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,64 @@
-export const Pagination = () => {};
+import { FC } from 'react';
+
+export const Pagination: FC = () => {
+  return (
+    <>
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+          >
+            «
+          </a>
+        </li>
+        <li className="page-item active">
+          <a data-cy="pageLink" className="page-link" href="#1">1</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#2">2</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#3">3</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#4">4</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#5">5</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#6">6</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#7">7</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#8">8</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#9">9</a>
+        </li>
+        <li className="page-item">
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li data-cy="item">Item 1</li>
+        <li data-cy="item">Item 2</li>
+        <li data-cy="item">Item 3</li>
+        <li data-cy="item">Item 4</li>
+        <li data-cy="item">Item 5</li>
+      </ul>
+    </>
+  );
+};
