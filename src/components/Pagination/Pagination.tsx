3880bb31605461a970f7bd1601b4456097402805
Yevhen Veselov <veselovyevhen@gmail.com>
2023-01-31 20:49:06 +0200

    react_pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,80 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pages = getNumbers(1, totalPages);
+
+  const handleChangePrev = () => (
+    currentPage !== 1
+      ? onPageChange(currentPage - 1)
+      : onPageChange(currentPage)
+  );
+
+  const handleChangeNext = () => (
+    currentPage !== totalPages
+      ? onPageChange(currentPage + 1)
+      : onPageChange(currentPage)
+  );
+
+  return (
+    <ul className="pagination">
+      <li
+        className={classNames('page-item',
+          { disabled: currentPage === 1 })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          onClick={handleChangePrev}
+          aria-disabled={currentPage === 1}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          key={page}
+          className={classNames('page-item',
+            { active: page === currentPage })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li
+        className={classNames('page-item',
+          { disabled: currentPage === totalPages })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          onClick={handleChangeNext}
+          aria-disabled={currentPage === totalPages}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
