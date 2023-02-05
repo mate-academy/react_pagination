4434fece74c2b5cac4e5e2a250455ce81102eb8a
Anna Inozemets <inoznetta@gmail.com>
2023-02-05 02:05:27 +0200

    Pagination was created

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,87 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPageCount = Math.ceil(total / perPage);
+  const pages: number[] = [];
+
+  for (let i = 1; i <= totalPageCount; i += 1) {
+    pages.push(i);
+  }
+
+  const onNext = () => {
+    if (currentPage === totalPageCount) {
+      return;
+    }
+
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    if (currentPage === 1) {
+      return;
+    }
+
+    onPageChange(currentPage - 1);
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={classNames('page-item', { disabled: currentPage === 1 })}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={onPrevious}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          className={classNames(
+            'page-item',
+            { active: currentPage === page },
+          )}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li className={classNames(
+        'page-item',
+        { disabled: currentPage === totalPageCount },
+      )}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === totalPageCount}
+          onClick={onNext}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
