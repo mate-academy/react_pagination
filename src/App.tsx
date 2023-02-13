47cac2df2a1fe4da28905d9c88c7b8fbfda50142
valntyn <romanenko.valentyn1@gmail.com>
2023-02-13 12:47:43 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ItemsPage } from './Pages';
 
 export const App = () => (
-  <Routes>
-    <Route path="/">
-      <Route index element={<ItemsPage />} />
-    </Route>
-  </Routes>
+  <ItemsPage />
 );
