72ec77e4ef3e5de746bde38d0cf77bdfa57f37ed
Deniskakaka <krollik1000000@gmail.com>
2023-02-19 22:21:47 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 import { getNumbers } from './utils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,12 +8,33 @@ const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [selecetCount, setSelectCount] = useState(5);
+  const [itemsCount, setItemCount] = useState(items.length / +selecetCount + 1);
+  const [list, setList] = useState(items.slice(0, selecetCount));
+  const [step, setStep] = useState(1);
+
+  const onChangeSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectCount(+event.target.value);
+    setStep(1);
+    setItemCount(Math.ceil(items.length / +event.target.value));
+  };
+
+  const renderSteps = useMemo(() => {
+    return [...items].slice(0, itemsCount);
+  }, [itemsCount]);
+
+  const returnNumber = (str: string) => str.replace(/\D/g, '');
+
+  useEffect(() => {
+    setList(items.slice(0, selecetCount));
+  }, [selecetCount]);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${step} (items ${returnNumber(list[0])} - ${returnNumber(list[list.length - 1])} of ${items.length})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +43,8 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={selecetCount}
+            onChange={onChangeSelect}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,62 +58,19 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        renderSteps={renderSteps}
+        setStep={setStep}
+        setList={setList}
+        selecetCount={selecetCount}
+        step={step}
+        items={items}
+        returnNumber={returnNumber}
+      />
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {
+          list.map((el: string) => <li data-cy="item" key={el}>{el}</li>)
+        }
       </ul>
     </div>
   );
