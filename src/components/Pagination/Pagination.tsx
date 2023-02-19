6455802e1f67be3ae95a2063568c80fa52abf35d
Andrii Medintsev <medintsev.andrew@gmail.com>
2023-02-19 21:03:02 +0200

    Pagination was implemented

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,92 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (pageNum: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange = () => {},
+}) => {
+  const pagesToDisplay
+    = [...Array(Math.ceil(total / perPage))].map((_, i) => i + 1);
+
+  const prevButtonhandler = () => {
+    if (currentPage === 1) {
+      return;
+    }
+
+    onPageChange(currentPage - 1);
+  };
+
+  const nextButtonHandler = () => {
+    if (currentPage === pagesToDisplay.length) {
+      return;
+    }
+
+    onPageChange(currentPage + 1);
+  };
+
+  return (
+    <ul className="pagination">
+      <li
+        className={classNames(
+          'page-item',
+          { disabled: currentPage === 1 },
+        )}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={prevButtonhandler}
+        >
+          «
+        </a>
+      </li>
+
+      {pagesToDisplay.map((page) => (
+        <li
+          className={classNames(
+            'page-item',
+            { active: currentPage === page },
+          )}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={classNames(
+          'page-item',
+          { disabled: currentPage === pagesToDisplay.length },
+        )}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === pagesToDisplay.length}
+          onClick={nextButtonHandler}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
