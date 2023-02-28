b25fcdfd1233f6f3767a35d01782871160b263ed
OksanaBaloh <obaloh1108@gmail.com>
2023-02-28 16:51:09 +0200

    Solution without React Router

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,83 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import React from 'react';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange:(page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pageAmount = Math.ceil(total / perPage);
+  const pages = getNumbers(1, pageAmount);
+
+  const equalFirst = currentPage === 1;
+  const equalLast = currentPage === pageAmount;
+
+  return (
+    <ul className="pagination">
+      <li className={classNames('page-item', {
+        disabled: equalFirst,
+      })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href={`#${currentPage}`}
+          aria-disabled={equalFirst}
+          onClick={() => {
+            if (!equalFirst) {
+              onPageChange(currentPage - 1);
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {pages.map(page => (
+        <li
+          className={classNames('page-item', {
+            active: page === currentPage,
+          })}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${currentPage}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li className={classNames('page-item', {
+        disabled: equalLast,
+      })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href={`#${currentPage}`}
+          aria-disabled={equalLast}
+          onClick={() => {
+            if (!equalLast) {
+              onPageChange(currentPage + 1);
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
