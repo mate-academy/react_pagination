34167889696c24358d70a716b444d776c7a4b093
Leonid <leonidbondarchuk6@gmail.com>
2023-05-10 22:51:14 +0300

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,51 @@
-export const Pagination = () => {};
+type Props = {
+  pageList: JSX.Element[]
+  itemList: JSX.Element[]
+  active: number
+  setActive: (number : number) => void
+};
+
+export const Pagination: React.FC<Props> = ({
+  pageList,
+  itemList,
+  active,
+  setActive,
+}) => {
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={`page-item ${active === 1 ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={active === 1}
+            onClick={() => setActive(active - 1)}
+          >
+            «
+          </a>
+        </li>
+        {pageList}
+        <li
+          className={`page-item ${active === pageList.length ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={active === pageList.length}
+            onClick={() => setActive(active + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {itemList}
+      </ul>
+
+    </>
+  );
+};
