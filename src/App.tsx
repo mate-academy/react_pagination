31882651e066f490d7268c8df245a6086e2a22f2
Ihor Zakharov <ihor.o.zakharov@gmail.com>
2023-05-19 16:11:01 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+const items = getNumbers(1, 42).map((n) => `Item ${n}`);
+
+const perPageOptions: number[] = [3, 5, 10, 20];
 
 export const App: React.FC = () => {
+  const [current, setCurrent] = useState(1);
+  const [perPage, setPerPage] = useState(perPageOptions[0]);
+
+  const onPageChange = useCallback((page: number) => {
+    setCurrent(page);
+  }, []);
+
+  const onPerPageChange = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setPerPage(+event.target.value);
+      setCurrent(1);
+    },
+    [],
+  );
+
+  const lastVisibleItemIndex = useMemo(
+    () => current * perPage,
+    [current, perPage],
+  );
+
+  const firstVisibleItemIndex = useMemo(
+    () => lastVisibleItemIndex - perPage,
+    [lastVisibleItemIndex, perPage],
+  );
+
+  const itemsLength = useMemo(() => items.length, [items]);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        Page
+        {' '}
+        {current}
+        {' '}
+        (
+        {`items ${firstVisibleItemIndex} - ${Math.min(lastVisibleItemIndex, itemsLength)} of ${itemsLength}`}
+        )
+
       </p>
 
       <div className="form-group row">
@@ -21,11 +57,14 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPage}
+            onChange={(event) => onPerPageChange(event)}
           >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            {perPageOptions.map((option) => (
+              <option value={`${option}`} key={option}>
+                {option}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -34,62 +73,22 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={itemsLength}
+        perPage={perPage}
+        currentPage={current}
+        onChange={onPageChange}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {items
+          .slice(firstVisibleItemIndex, lastVisibleItemIndex)
+          .map((item) => (
+            <li data-cy="item">
+              Item
+              {item}
+            </li>
+          ))}
       </ul>
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,105 @@
-export const Pagination = () => {};
+import {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
+import classNames from 'classnames';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onChange,
+}) => {
+  const [current, setCurrent] = useState<number>(1);
+
+  useEffect(() => {
+    setCurrent(currentPage);
+  }, [currentPage]);
+
+  const pagesAmount = useMemo(
+    () => Math.ceil(total / perPage),
+    [total, perPage],
+  );
+  const pages = useMemo(() => getNumbers(1, pagesAmount), [pagesAmount]);
+
+  const leftButtonDisabled = useMemo(() => current === 1, [current]);
+  const rightButtonDisabled = useMemo(
+    () => current === pagesAmount,
+    [current, pagesAmount],
+  );
+
+  const handleChange = useCallback(() => {
+    setCurrent((prevState) => {
+      onChange(prevState);
+
+      return prevState;
+    });
+  }, [onChange]);
+
+  const handleLeftButtonClick = useCallback(() => {
+    if (!leftButtonDisabled) {
+      setCurrent(current - 1);
+
+      handleChange();
+    }
+  }, [current, leftButtonDisabled]);
+
+  const handleRightButtonCLick = useCallback(() => {
+    if (!rightButtonDisabled) {
+      setCurrent(current + 1);
+
+      handleChange();
+    }
+  }, [current, rightButtonDisabled]);
+
+  return (
+    <ul className="pagination">
+      <li className={classNames('page-item', { disabled: leftButtonDisabled })}>
+        <a
+          data-cy="prevLink"
+          className="page-link "
+          href="#prev"
+          aria-disabled="true"
+          onClick={handleLeftButtonClick}
+        >
+          «
+        </a>
+      </li>
+      {pages.map((page) => (
+        <li className={classNames('page-item', { active: page === current })}>
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => {
+              setCurrent(page);
+              handleChange();
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li
+        className={classNames('page-item', { disabled: rightButtonDisabled })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled="false"
+          onClick={handleRightButtonCLick}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
