03ea570f929e3e29f8c440815020ae536336888c
Kocherha Roman <f.your.d.2017@gmail.com>
2023-05-25 11:50:33 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { getNumbers } from './utils';
 import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
@@ -16,7 +15,7 @@ export const App: React.FC = () => {
   const fromItem = Number(perPage) * currentPage + 1;
   const toItem = fromItem + Number(perPage) - 1;
 
-  const handleChangCountItem = (
+  const handleChangeCountItem = (
     { target }: React.ChangeEvent<HTMLSelectElement>,
   ) => {
     setPerPage(target.value);
@@ -51,7 +50,7 @@ export const App: React.FC = () => {
             id="perPageSelector"
             className="form-control"
             value={perPage}
-            onChange={handleChangCountItem}
+            onChange={handleChangeCountItem}
           >
             {options.map(item => (
               <option key={item} value={item}>{item}</option>
@@ -64,7 +63,6 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
       <Pagination
         total={items}
         perPage={perPage}
