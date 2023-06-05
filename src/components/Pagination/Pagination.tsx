11cc70ce3e631369a14e62318a2322c7afcb7905
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-06-05 21:15:52 +0200

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,86 @@
-export const Pagination = () => {};
+import { FC } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { getSearchWith } from '../../utils/searchHelper';
+
+type Props = {
+  currentPage: string,
+  itemsToShow: string[],
+  pages: string[],
+};
+
+export const Pagination: FC<Props> = ({ pages, currentPage, itemsToShow }) => {
+  const [searchParams] = useSearchParams();
+  const isFirstPage = currentPage === pages[0];
+  const isLastPage = currentPage === pages[pages.length - 1];
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={classNames('page-item', {
+            disabled: isFirstPage,
+          })}
+        >
+          <Link
+            data-cy="nextLink"
+            className="page-link"
+            to={{
+              search: getSearchWith(searchParams, { page: `${+currentPage - 1}` }),
+            }}
+            aria-disabled={isFirstPage ? 'true' : 'false'}
+          >
+            «
+          </Link>
+        </li>
+
+        {pages.map(page => (
+          <li
+            key={page}
+            className={classNames('page-item', {
+              active: page === currentPage,
+            })}
+          >
+            <Link
+              data-cy="pageLink"
+              className="page-link"
+              to={{
+                search: getSearchWith(searchParams, { page }),
+              }}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+
+        <li
+          className={classNames('page-item', {
+            disabled: isLastPage,
+          })}
+        >
+          <Link
+            data-cy="nextLink"
+            className="page-link"
+            to={{
+              search: getSearchWith(searchParams, { page: `${+currentPage + 1}` }),
+            }}
+            aria-disabled={isLastPage ? 'true' : 'false'}
+          >
+            »
+          </Link>
+        </li>
+      </ul>
+
+      <ul>
+        {itemsToShow.map(item => (
+          <li
+            key={item}
+            data-cy="item"
+          >
+            {`Item ${item}`}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root'),
+);
