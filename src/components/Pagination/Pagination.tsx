99c5a93385e058fea5adfeb3a7a260d3a7b7a182
Kateryna Sokur <k.o.sokur@gmail.com>
2023-06-26 16:25:44 +0200

    add solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,69 @@
-export const Pagination = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: CallableFunction,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pages = getNumbers(1, Math.ceil(total / perPage));
+
+  return (
+    <ul className="pagination">
+      <li className={classNames('page-item', {
+        disabled: currentPage === 1,
+      })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled="true"
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          key={page}
+          className={classNames('page-item', {
+            active: page === currentPage,
+          })}
+        >
+          <a
+            onClick={() => onPageChange(page)}
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li className={classNames('page-item', {
+        disabled: currentPage === pages.length,
+      })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled="false"
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
