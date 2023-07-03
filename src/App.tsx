49387c2cb57d8337aa2a42d4ac45c68bacf59cf0
Roman Kvas <fallenmadara@192.168.1.11>
2023-07-03 08:56:48 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,82 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  // const [totalItems] = useState<string[]>(items);
+  // const [itemsPerPage, setItemsPerPage] = useState(5);
+  // const [currentPage, setCurrentPage] = useState(0);
+  const [isSearchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(isSearchParams.get('page'));
+  const perPage = Number(isSearchParams.get('perPage') || 5);
+
+  const getSearchWith = (
+    searchParams: URLSearchParams,
+    params: { [key: string]: string[] | string | null },
+  ) => {
+    Object.entries(params).forEach(([key, value]) => {
+      if (value === null) {
+        searchParams.delete(key);
+      } else if (Array.isArray(value)) {
+        searchParams.delete(key);
+
+        value.forEach(el => {
+          searchParams.append(key, el);
+        });
+      } else {
+        searchParams.set(key, value);
+      }
+    });
+
+    return searchParams.toString();
+  };
+
+  // const firstItem = itemsPerPage * currentPage;
+  const firstItem = perPage * page;
+  // const lastItem = Math.min(
+  //   (currentPage + 1) * itemsPerPage,
+  //   totalItems.length,
+  // );
+  const lastItem = Math.min(
+    (page + 1) * perPage,
+    items.length,
+  );
+
+  const currentItems = items.slice(firstItem, lastItem);
+
+  const onPageChange = ((currPage: number) => {
+    // setCurrentPage(currPage);
+    setSearchParams(
+      getSearchWith(isSearchParams, { page: currPage.toString() || null }),
+    );
+  });
+
+  const changeItemPerPage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    // updateSearchParams({ perPage: e.target.value || null });
+    // setItemsPerPage(+e.target.value);
+    // setCurrentPage(0);
+    setSearchParams(
+      getSearchWith(isSearchParams, {
+        perPage: e.target.value || null,
+        page: '0',
+      }),
+    );
+    // updateSearchParams({ page: 0 || null });
+  };
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${page + 1} (items ${(firstItem) + 1} - ${lastItem} of ${items.length})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +85,8 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPage}
+            onChange={changeItemPerPage}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,63 +100,13 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination
+        currentItems={currentItems}
+        totalItems={items}
+        currentPage={page}
+        onPageChange={onPageChange}
+        itemPerPage={perPage}
+      />
     </div>
   );
 };
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,80 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  totalItems: string[];
+  currentItems: string[];
+  currentPage: number;
+  onPageChange: (page: number) => void;
+  itemPerPage: number;
+};
+
+export const Pagination: React.FC<Props> = ({
+  totalItems,
+  itemPerPage,
+  onPageChange,
+  currentPage,
+  currentItems,
+}) => {
+  const location = useLocation();
+  const totalPages = Math.ceil(totalItems.length / +itemPerPage);
+  const pageNumbers = getNumbers(1, totalPages);
+
+  return (
+    <>
+      <ul className="pagination">
+        <>
+          <li className={classNames('page-item',
+            { disabled: currentPage <= 0 })}
+          >
+            <Link
+              data-cy="prevLink"
+              className="page-link"
+              to={{ pathname: '/prev' }}
+              aria-disabled={currentPage <= 0}
+              onClick={() => onPageChange(currentPage - 1)}
+            >
+              «
+            </Link>
+          </li>
+          {pageNumbers.map(number => (
+            <li
+              key={number}
+              className={classNames('page-item',
+                { active: currentPage === (number - 1) })}
+            >
+              <Link
+                data-cy="pageLink"
+                className="page-link"
+                to={{ pathname: `/${number + 1}`, search: location.search }}
+                onClick={() => onPageChange(number - 1)}
+              >
+                {number}
+              </Link>
+            </li>
+          ))}
+          <li className={classNames('page-item',
+            { disabled: currentPage >= totalPages - 1 })}
+          >
+            <Link
+              data-cy="nextLink"
+              className="page-link"
+              to="next"
+              aria-disabled={currentPage >= totalPages - 1}
+              onClick={() => onPageChange(currentPage + 1)}
+            >
+              »
+            </Link>
+          </li>
+        </>
+      </ul>
+
+      <ul>
+        {currentItems.map(item => (
+          <li data-cy="item" key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
