dcf56a6c0b67a4eadf405a0afe2cf19f53e79b6c
alex <cebanoleksandr@gmail.com>
2023-07-06 22:17:26 +0300

    solution7

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,11 @@ export const App: React.FC<Props> = () => {
 
   const partOfItems = getPartOfItems(currentPage, perPage);
 
+  const perPageHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPerPage(+event.target.value);
+    setCurrentPage(1);
+  }
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
@@ -48,7 +53,7 @@ export const App: React.FC<Props> = () => {
             id="perPageSelector"
             className="form-control"
             value={perPage}
-            onChange={(event) => setPerPage(+event.target.value)}
+            onChange={perPageHandler}
           >
             <option value="3">3</option>
             <option value="5">5</option>
