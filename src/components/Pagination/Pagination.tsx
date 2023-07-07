c578f50fbe9d2a86bbe642f3022a4b9043bb4f5d
IvanVaverchak <ivanvaverchak@2000gmail.com>
2023-07-07 13:51:57 +0300

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,79 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+
+type Props = {
+  total: number,
+  perPage: number
+  currentPage: number
+  onPageChange: (page: number) => void,
+  arrOfPages: number[],
+};
+
+export const Pagination: React.FC<Props> = ({
+  currentPage,
+  onPageChange,
+  arrOfPages,
+}) => {
+  const rightArrowHandler = () => {
+    if (currentPage < arrOfPages[arrOfPages.length - 1]) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const leftArrowHandler = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={classNames(
+        'page-item', { disabled: currentPage === 1 },
+      )}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1 && 'true'}
+          onClick={leftArrowHandler}
+        >
+          «
+        </a>
+      </li>
+      {arrOfPages.map(num => (
+        <li
+          className={classNames(
+            'page-item', { active: num === currentPage },
+          )}
+          key={num}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${num}`}
+            onClick={() => {
+              onPageChange(num);
+            }}
+          >
+            {num}
+          </a>
+        </li>
+      ))}
+      <li className={classNames(
+        'page-item', { disabled: currentPage === arrOfPages.length },
+      )}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === arrOfPages.length && 'true'}
+          onClick={rightArrowHandler}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
