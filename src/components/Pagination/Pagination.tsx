14225372970b33b217c423d90c2ab65a3428f548
Petro Lozynskyi <petrolozynskyi01@gmail.com>
2023-07-07 09:12:58 +0300

    update

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,61 @@
-export const Pagination = () => {};
+import React from 'react';
+
+interface PaginationProps {
+  total: number;
+  currentPage: number;
+  perPage: number;
+  onPageChange: (pageNumber: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total = 42,
+  currentPage,
+  perPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pageNumbers = Array.from({ length: totalPages },
+    (_, index) => index + 1);
+
+  return (
+    <ul className="pagination">
+      <li className="page-item disabled">
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled="true"
+        >
+          «
+        </a>
+      </li>
+
+      {pageNumbers.map((pageNumber) => (
+        <li
+          key={pageNumber}
+          className={`page-item ${pageNumber === currentPage ? 'active' : ''}`}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${pageNumber}`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </a>
+        </li>
+      ))}
+
+      <li className="page-item">
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled="false"
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
