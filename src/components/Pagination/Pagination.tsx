fecd5b803ab361c1d40d3356dce66a4e65af3083
Olena <pampam32121@gmail.com>
2023-07-11 21:26:19 +0300

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,106 @@
-export const Pagination = () => {};
+import React from 'react';
+import classNames from 'classnames';
+
+type Callback = (page: number) => number;
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number | Callback) => void,
+  currentItems: string[]
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+  currentItems,
+}) => {
+  const totalPages: number = Math.ceil(total / perPage);
+  const pagesArray = Array.from(
+    { length: totalPages },
+    (_value, index) => index + 1,
+  );
+
+  function moveToPreviousPage() {
+    if (currentPage === 1) {
+      return;
+    }
+
+    onPageChange(prevPage => prevPage - 1);
+  }
+
+  function moveToNextPage() {
+    if (currentPage === totalPages) {
+      return;
+    }
+
+    onPageChange(prevPage => prevPage + 1);
+  }
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={classNames('page-item', {
+            disabled: currentPage === 1,
+          })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={moveToPreviousPage}
+          >
+            «
+          </a>
+        </li>
+
+        {pagesArray.map((pageNo: number) => (
+          <li
+            key={pageNo}
+            className={classNames('page-item', {
+              active: pageNo === currentPage,
+            })}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${pageNo}`}
+              onClick={() => onPageChange(pageNo)}
+            >
+              {pageNo}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={classNames('page-item', {
+            disabled: currentPage === totalPages,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === totalPages}
+            onClick={moveToNextPage}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        {currentItems.map(item => (
+          <li data-cy="item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
