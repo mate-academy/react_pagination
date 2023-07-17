e69e9a2de1b1142a8a18985c6d01ba9108db6d9b
Konstantin <ivanchenkomypost@gmail.com>
2023-07-17 13:54:48 +0300

    requested changes added

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -18,6 +18,14 @@ export const Pagination: React.FC<Props> = ({
 }) => {
   const totalPages = Math.ceil(total / perPage);
 
+  const prevPageHandler = () => (
+    currentPage !== 1 && onPageChange(currentPage - 1)
+  );
+
+  const nextPageHandler = () => (
+    currentPage !== totalPages && onPageChange(currentPage + 1)
+  );
+
   return (
     <ul className="pagination">
       <li className={cn('page-item', { disabled: currentPage === 1 })}>
@@ -26,9 +34,7 @@ export const Pagination: React.FC<Props> = ({
           className="page-link"
           href="#prev"
           aria-disabled={currentPage === 1}
-          onClick={() => (
-            currentPage !== 1 && onPageChange(currentPage - 1)
-          )}
+          onClick={prevPageHandler}
         >
           «
         </a>
@@ -54,9 +60,7 @@ export const Pagination: React.FC<Props> = ({
           className="page-link"
           href="#next"
           aria-disabled={currentPage === totalPages}
-          onClick={() => (
-            currentPage !== totalPages && onPageChange(currentPage + 1)
-          )}
+          onClick={nextPageHandler}
         >
           »
         </a>
