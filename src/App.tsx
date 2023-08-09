3175f7f175d6d6923b879a552a655e2a1591cc2b
Valera Viachalo <valeravyachalo@gmail.com>
2023-08-09 11:10:30 +0300

    pagination task added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Pagination } from './components/Pagination';
 import { getNumbers } from './utils';
+import { ItemList } from './components/ItemList';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
+const totalItems = items.length;
 
 export const App: React.FC = () => {
+  const [visiblePages, setVisiblePages] = useState(5);
+  const [selectPage, setSelectPage] = useState(1);
+
+  const start = (selectPage * visiblePages) - visiblePages;
+  const end = Math.min(start + visiblePages, totalItems);
+
+  const handleSelectPage = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setVisiblePages(+event.target.value);
+    setSelectPage(1);
+  };
+
+  const handlePageChange = (page: number) => {
+    if (page !== selectPage) {
+      setSelectPage(page);
+    }
+  };
+
   return (
     <div className="container">
-      <h1>Items with Pagination</h1>
+      <h1>Item with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${selectPage} (items ${start + 1} - ${end} of ${totalItems})`}
       </p>
 
       <div className="form-group row">
         <div className="col-3 col-sm-2 col-xl-1">
           <select
+            value={visiblePages}
+            onChange={handleSelectPage}
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
@@ -34,63 +55,18 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination
+        total={totalItems}
+        perPage={visiblePages}
+        currentPage={selectPage}
+        onPageChange={handlePageChange}
+      />
+
+      <ItemList
+        start={start}
+        end={end}
+        items={items}
+      />
     </div>
   );
 };
---
+++ src/components/Pagination/PaginationButton/PaginationButton.tsx
@@ -0,0 +1,38 @@
+import classNames from 'classnames';
+import React from 'react';
+
+type Props = {
+  dataCy: string;
+  label: string;
+  active: boolean;
+  disabled: boolean;
+  onClickedEvent: () => void;
+  ariaDisabled?: boolean;
+};
+
+export const PaginationButton: React.FC<Props> = ({
+  dataCy,
+  label,
+  active,
+  disabled,
+  onClickedEvent,
+  ariaDisabled = true,
+}) => {
+  return (
+    <li className={classNames('pagi-item', { disabled, active })}>
+      <button
+        type="button"
+        data-cy={dataCy}
+        className="page-link"
+        onClick={onClickedEvent}
+        aria-disabled={ariaDisabled ? 'true' : 'false'}
+      >
+        {label}
+      </button>
+    </li>
+  );
+};
+
+PaginationButton.defaultProps = {
+  ariaDisabled: true,
+};
