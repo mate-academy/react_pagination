f537bc3bfc7e6074f52e1d9a5b6b99dafdd142b2
your_username <web.shepel@gmail.com>
2023-08-15 13:59:02 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
+const params = new URLSearchParams(window.location.search);
+
 export const App: React.FC = () => {
+  const [
+    perPage,
+    setPerPage,
+  ] = useState(Number(params.get('perPage')) || 5);
+  const [
+    currentPage,
+    setCurrentPage,
+  ] = useState(Number(params.get('page')) || 1);
+  const total = items.length;
+  const firstItemPerPage = currentPage * perPage - perPage + 1;
+  const lastItemPerPage = Math.min(total, currentPage * perPage);
+
+  function changeUrlParams(pageParam: number, perPageParam: number) {
+    params.set('page', String(pageParam));
+    params.set('perPage', String(perPageParam));
+
+    window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
+  }
+
+  function onPageChange(newPage: number) {
+    changeUrlParams(newPage, perPage);
+    setCurrentPage(newPage);
+  }
+
+  function onPerPageChange(newPerPage: number) {
+    changeUrlParams(1, newPerPage);
+    setPerPage(newPerPage);
+    setCurrentPage(1);
+  }
+
+  useEffect(() => {
+    changeUrlParams(currentPage, perPage);
+  }, []);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${currentPage} (items ${firstItemPerPage} - ${lastItemPerPage} of ${total})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +57,8 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            onChange={(e) => onPerPageChange(Number(e.target.value))}
+            value={perPage}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,62 +72,17 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={total}
+        perPage={perPage}
+        currentPage={currentPage}
+        onPageChange={(page) => onPageChange(page)}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {items
+          .slice(firstItemPerPage - 1, lastItemPerPage)
+          .map(el => <li data-cy="item" key={el}>{el}</li>)}
       </ul>
     </div>
   );
