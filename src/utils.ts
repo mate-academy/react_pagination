33d51a0ac7b1c585e3ec7e8b5bdf5e604641b0dc
Antonina Klishch <tonya91t@gmail.com>
2023-08-19 16:30:25 +0300

    Solution

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,31 @@ export function getNumbers(from: number, to: number): number[] {
 
   return numbers;
 }
+
+export function getCountPages(totalItems: number, countItemsInGroup: number) {
+  return Math.ceil(totalItems / countItemsInGroup);
+}
+
+export function getFirstItemGroup(
+  currentPage: number,
+  countItems: number,
+  total = 42,
+) {
+  const number = (currentPage - 1) * countItems;
+
+  return number <= total
+    ? number
+    : 1;
+}
+
+export function getLastItemGroup(
+  currentPage: number,
+  countItems: number,
+  total = 42,
+) {
+  const number = (currentPage - 1) * countItems + countItems;
+
+  return number <= total
+    ? number
+    : total;
+}
