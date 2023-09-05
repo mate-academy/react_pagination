5379b1c19ca0b1e6d8476a549de3d350770a570e
Anton Tsurkan <wesses1999@gmail.com>
2023-09-05 10:43:59 +0300

    add map rendering perPages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import { Pagination } from './components/Pagination';
 
 const TOTAL = 42;
+const PerPagesList = [3, 5, 10, 20];
 
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState(5);
+  const [perPage, setPerPage] = useState(PerPagesList[1]);
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
@@ -24,44 +25,20 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
-            defaultValue={5}
+            defaultValue={PerPagesList[1]}
           >
-            <option
-              value="3"
-              onClick={() => {
-                setPerPage(3);
-                setCurrentPage(1);
-              }}
-            >
-              3
-            </option>
-            <option
-              value="5"
-              onClick={() => {
-                setPerPage(5);
-                setCurrentPage(1);
-              }}
-            >
-              5
-            </option>
-            <option
-              value="10"
-              onClick={() => {
-                setPerPage(10);
-                setCurrentPage(1);
-              }}
-            >
-              10
-            </option>
-            <option
-              value="20"
-              onClick={() => {
-                setPerPage(20);
-                setCurrentPage(1);
-              }}
-            >
-              20
-            </option>
+            {PerPagesList.map(perP => (
+              <option
+                key={perP}
+                value={perP}
+                onClick={() => {
+                  setPerPage(perP);
+                  setCurrentPage(1);
+                }}
+              >
+                {perP}
+              </option>
+            ))}
           </select>
         </div>
         <label htmlFor="perPageSelector" className="col-form-label col">
