c7598913a79dae6b943a2121754dcd963bc668cc
Klaudia <kklis@interia.pl>
2023-09-08 15:00:14 +0200

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+const items = getNumbers(1, 42);
 
 export const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+
+  const onPageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleChange: React.ChangeEventHandler<HTMLSelectElement>
+    = (event) => {
+      setCurrentPage(1);
+      setPerPage(+event.target.value);
+    };
+
+  const sortPages = (itemsPerPage: number) => {
+    const sortedPages = [];
+
+    for (let i = 0; i < items.length; i += itemsPerPage) {
+      sortedPages.push(items.slice(i, i + itemsPerPage));
+    }
+
+    return sortedPages;
+  };
+
+  const firstItemOnPage = sortPages(perPage)[currentPage - 1].at(0);
+  const lastItemOnPage = sortPages(perPage)[currentPage - 1].at(-1);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${currentPage} (items ${firstItemOnPage} - ${lastItemOnPage} of 42)`}
       </p>
 
       <div className="form-group row">
         <div className="col-3 col-sm-2 col-xl-1">
           <select
+            name="perPageSelector"
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            onChange={handleChange}
+            defaultValue={5}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -33,63 +61,23 @@ export const App: React.FC = () => {
           items per page
         </label>
       </div>
-
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={42}
+        perPage={perPage}
+        currentPage={currentPage}
+        onPageChange={onPageChange}
+      />
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {sortPages(perPage)[currentPage - 1].map(number => {
+          return (
+            <li
+              key={number}
+              data-cy="item"
+            >
+              {`Item ${number}`}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
