a712ead8b210ce4eb77e34b8ad57cc049f476470
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-10-09 00:49:56 +0300

    fix

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -9,6 +9,12 @@ interface Props {
   onPageChange: (page: number) => void;
 }
 
+enum Buttons {
+  LeftArrow,
+  PageButton,
+  RightArrow,
+}
+
 export const Pagination: React.FC<Props> = ({
   total,
   perPage,
@@ -18,19 +24,47 @@ export const Pagination: React.FC<Props> = ({
   const currentPagesNumber = Math.ceil(total / perPage);
   const pagesArray = getNumbers(1, currentPagesNumber);
 
+  const isFirstPage = () => currentPage === 1;
+  const isCurrentPage = (page: number) => page === currentPage;
+  const isLastPage = () => currentPage === currentPagesNumber;
+
+  function handleClick(button: Buttons, page: number = currentPage) {
+    switch (button) {
+      case Buttons.LeftArrow:
+        return () => {
+          if (!isFirstPage()) {
+            onPageChange(currentPage - 1);
+          }
+        };
+
+      case Buttons.PageButton:
+        return () => {
+          if (!isCurrentPage(page)) {
+            onPageChange(page);
+          }
+        };
+
+      case Buttons.RightArrow:
+        return () => {
+          if (currentPage !== currentPagesNumber) {
+            onPageChange(currentPage + 1);
+          }
+        };
+
+      default:
+        return () => {};
+    }
+  }
+
   return (
     <ul className="pagination">
-      <li className={cn('page-item', { disabled: currentPage === 1 })}>
+      <li className={cn('page-item', { disabled: isFirstPage() })}>
         <a
           data-cy="prevLink"
           className="page-link"
           href="#prev"
-          aria-disabled={currentPage === 1 ? 'true' : 'false'}
-          onClick={() => {
-            if (currentPage !== 1) {
-              onPageChange(currentPage - 1);
-            }
-          }}
+          aria-disabled={isFirstPage() ? 'true' : 'false'}
+          onClick={handleClick(Buttons.LeftArrow)}
         >
           «
         </a>
@@ -38,18 +72,14 @@ export const Pagination: React.FC<Props> = ({
 
       {pagesArray.map(page => (
         <li
-          className={cn('page-item', { active: page === currentPage })}
+          className={cn('page-item', { active: isCurrentPage(page) })}
           key={page}
         >
           <a
             data-cy="pageLink"
             className="page-link"
             href={`#${page}`}
-            onClick={() => {
-              if (page !== currentPage) {
-                onPageChange(page);
-              }
-            }}
+            onClick={handleClick(Buttons.PageButton, page)}
           >
             {page}
           </a>
@@ -58,19 +88,15 @@ export const Pagination: React.FC<Props> = ({
 
       <li className={
         cn('page-item',
-          { disabled: currentPage === currentPagesNumber })
+          { disabled: isLastPage() })
       }
       >
         <a
           data-cy="nextLink"
           className="page-link"
           href="#next"
-          aria-disabled={currentPage === currentPagesNumber ? 'true' : 'false'}
-          onClick={() => {
-            if (currentPage !== currentPagesNumber) {
-              onPageChange(currentPage + 1);
-            }
-          }}
+          aria-disabled={isLastPage() ? 'true' : 'false'}
+          onClick={handleClick(Buttons.RightArrow)}
         >
           »
         </a>
