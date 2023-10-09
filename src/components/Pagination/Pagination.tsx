d7e70582af7de69ff1d2db070e5a2f8f263a8430
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-10-09 16:15:16 +0300

    react pagination was created

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,123 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  let pagesCount = 0;
+
+  const pages: number[] = [];
+  const itemsArr: number[] = [];
+
+  for (let i = 1; i <= perPage; i += 1) {
+    const resultItem = i + perPage * (currentPage - 1);
+
+    itemsArr.push(resultItem);
+  }
+
+  for (let i = total; i > 0; i -= perPage) {
+    pagesCount += 1;
+    pages.push(pagesCount);
+  }
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={cn('page-item', {
+          disabled: currentPage === 1,
+        })}
+        >
+          {currentPage === 1 ? (
+            <a
+              data-cy="prevLink"
+              className="page-link"
+              href="#prev"
+              aria-disabled="true"
+            >
+              «
+            </a>
+          ) : (
+            <a
+              data-cy="prevLink"
+              className="page-link"
+              href="#prev"
+              aria-disabled="false"
+              onClick={() => {
+                onPageChange(currentPage - 1);
+              }}
+            >
+              «
+            </a>
+          )}
+        </li>
+        {pages.map(page => (
+          <li
+            className={cn('page-item', {
+              active: currentPage === page,
+            })}
+            key={page}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${page}`}
+              onClick={() => {
+                if (currentPage !== page) {
+                  onPageChange(page);
+                }
+              }}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+        <li className={cn('page-item', {
+          disabled: currentPage === pages[pages.length - 1],
+        })}
+        >
+          {currentPage !== pages[pages.length - 1] ? (
+            <a
+              data-cy="nextLink"
+              className="page-link"
+              href="#next"
+              aria-disabled="false"
+              onClick={() => {
+                onPageChange(currentPage + 1);
+              }}
+            >
+              »
+            </a>
+          ) : (
+            <a
+              data-cy="nextLink"
+              className="page-link"
+              href="#next"
+              aria-disabled="true"
+            >
+              »
+            </a>
+          )}
+        </li>
+      </ul>
+      <ul>
+        {itemsArr.map(item => (
+          item <= total && (
+            <li data-cy="item">
+              {`Item ${item}`}
+            </li>
+          )
+        ))}
+      </ul>
+    </>
+  );
+};
