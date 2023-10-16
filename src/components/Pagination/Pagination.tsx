c76e67c003368d9f52c5723a650939f6677ac37f
luka4ok <vitalik.shostak@gmail.com>
2023-10-16 10:32:56 +0300

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,89 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const paginationRange = Math.ceil(total / perPage);
+  const pages = getNumbers(1, paginationRange);
+
+  const prevDisabled = currentPage === 1;
+  const nextDisabled = currentPage === paginationRange;
+
+  const onNext = () => {
+    if (currentPage < paginationRange) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const onPrevious = () => {
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={cn('page-item', { disabled: prevDisabled })}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={prevDisabled}
+            onClick={onPrevious}
+          >
+            «
+          </a>
+        </li>
+
+        {pages.map((page) => (
+          <li
+            className={cn('page-item', { active: page === currentPage })}
+            key={page}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${page}`}
+              onClick={() => {
+                if (page !== currentPage) {
+                  onPageChange(page);
+                }
+              }}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={cn('page-item', {
+            disabled: nextDisabled,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={nextDisabled}
+            onClick={onNext}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
