83913eb9a3876bd78bb9aaa6e6521f49230fea1f
Yuliia Bakun <bakunyuliia@gmail.com>
2023-10-19 13:54:13 +0200

    task implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+type Item = {
+  id: number,
+  title: string,
+};
+
+const items = getNumbers(1, 42).map(n => `Item ${n}`);
+
+const preparedItems: Item[] = items
+  .map((item, index) => ({ id: index, title: item }));
+
+function getVisibleItems(allItems: Item[], start: number, end: number) {
+  return allItems.slice(start, end + 1);
+}
 
 export const App: React.FC = () => {
+  const [itemsOnPage, setitemsOnPage] = useState(5);
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  const start = (selectedPage - 1) * itemsOnPage;
+  const end = Math.min(start + itemsOnPage - 1, preparedItems.length - 1);
+
+  const visibleItems = getVisibleItems(preparedItems, start, end);
+
+  const handleOnPageChange = (pageNumber: number) => {
+    if (selectedPage !== pageNumber) {
+      setSelectedPage(pageNumber);
+    }
+  };
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${selectedPage} (items ${start + 1} - ${end + 1} of ${preparedItems.length})`}
       </p>
 
       <div className="form-group row">
@@ -21,6 +46,11 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            defaultValue={itemsOnPage}
+            onChange={event => {
+              setitemsOnPage(+event.target.value);
+              handleOnPageChange(1);
+            }}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,62 +64,17 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={preparedItems.length}
+        perPage={itemsOnPage}
+        currentPage={selectedPage}
+        onPageChange={handleOnPageChange}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {visibleItems.map(item => (
+          <li data-cy="item" key={item.id}>{item.title}</li>
+        ))}
       </ul>
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,69 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pages = Math.ceil(total / perPage);
+  const pagesArray = getNumbers(1, pages);
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item',
+        { disabled: currentPage === pagesArray[0] })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === pagesArray[0] ? 'true' : 'false'}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+
+      {pagesArray.map(page => (
+        <li
+          key={page}
+          className={cn('page-item', { active: currentPage === page })}
+          role="presentation"
+          onClick={() => onPageChange(page)}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li className={cn('page-item',
+        { disabled: currentPage === pagesArray[pagesArray.length - 1] })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === pagesArray[pagesArray.length - 1]
+            ? 'true' : 'false'}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
