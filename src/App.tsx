84e8da1d1d5a946dd12b9efd9908f2352b1e414a
elenagrasovskaya <elenagrasovskaya@gmail.com>
2023-10-21 15:27:15 +0300

    React TypeScript Pagination try 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { getNumbers } from './utils';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+import { pageOptions } from './utils';
+import { Pagination } from './components/Pagination';
+
+const total = 42;
 
 export const App: React.FC = () => {
+  const [perPage, setPerPage] = useState(5);
+  const [activePage, setActivePage] = useState(1);
+
+  const handlePageCountSelect = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setPerPage(Number(event.target.value));
+    setActivePage(1);
+  };
+
+  const lastPageCount = ((total - ((activePage - 1) * perPage)) < perPage)
+    ? (total - ((activePage - 1) * perPage))
+    : perPage;
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
-
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${activePage} (items ${(activePage - 1) * perPage + 1} - ${((activePage - 1) * perPage) + lastPageCount} of ${total})`}
       </p>
 
       <div className="form-group row">
@@ -21,11 +34,14 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPage}
+            onChange={handlePageCountSelect}
           >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            {pageOptions.map((option) => (
+              <option value={option} key={`${option}`}>
+                {option}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -33,64 +49,12 @@ export const App: React.FC = () => {
           items per page
         </label>
       </div>
-
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination
+        total={total} // total number of items to paginate
+        perPage={perPage} // number of items per page
+        currentPage={activePage} /* optional with 1 by default */
+        onPageChange={setActivePage}
+      />
     </div>
   );
 };
---
+++ src/types/ItemsPerPage.ts
@@ -0,0 +1,3 @@
+export interface ItemsPerPage<T> {
+  [key: string]: T[];
+}
