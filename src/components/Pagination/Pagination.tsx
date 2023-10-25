f69f86efae98d350b289d87188666f4feee6031b
Bahtijar <no4kar@gmail.com>
2023-10-25 21:37:21 +0200

    stage-1

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,74 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const pageNumbers
+    = getNumbers(1, Math.ceil(total / perPage));
+  const isFirstPage: boolean
+    = currentPage === pageNumbers[0];
+  const isLastPage: boolean
+    = currentPage === pageNumbers[pageNumbers.length - 1];
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item', {
+        disabled: isFirstPage,
+      })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={isFirstPage ? 'true' : 'false'}
+          onClick={() => isFirstPage || onPageChange(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+
+      {pageNumbers.map((pageNumber) => (
+        <li className={cn(
+          'page-item',
+          { active: pageNumber === currentPage },
+        )}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${pageNumber}`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </a>
+        </li>
+      ))}
+
+      <li className={cn('page-item', {
+        disabled: isLastPage,
+      })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={isLastPage ? 'true' : 'false'}
+          onClick={() => isLastPage || onPageChange(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
