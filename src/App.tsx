93bc32e2088267d5718533a0430075f3e5b5cf55
newqou <137334905+newqou@users.noreply.github.com>
2023-10-30 16:43:55 +0200

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { getNumbers } from './utils';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
+import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
+
+const FIRST_ITEM_NUMBER = 1;
+const LAST_ITEM_NUMBER = 42;
+
+const items = getNumbers(FIRST_ITEM_NUMBER, LAST_ITEM_NUMBER)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [countVisibleItems, setCountVisibleItems] = useState(5);
+  const [activePage, setActivePage] = useState(1);
+
+  const itemsStart = countVisibleItems * activePage - countVisibleItems
+    + FIRST_ITEM_NUMBER;
+
+  const itemsFinish = countVisibleItems * activePage > LAST_ITEM_NUMBER
+    ? LAST_ITEM_NUMBER
+    : countVisibleItems * activePage;
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {
+          `Page ${activePage} (items ${itemsStart} - ${itemsFinish} of ${LAST_ITEM_NUMBER})`
+        }
       </p>
 
       <div className="form-group row">
@@ -21,6 +37,11 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={countVisibleItems}
+            onChange={event => {
+              setCountVisibleItems(+event.target.value);
+              setActivePage(1);
+            }}
           >
             <option value="3">3</option>
             <option value="5">5</option>
@@ -34,63 +55,13 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul>
+      <Pagination
+        items={items}
+        countItemsPage={countVisibleItems}
+        currentPage={activePage}
+        setCurrentPage={setActivePage}
+        key={items.length}
+      />
     </div>
   );
 };
