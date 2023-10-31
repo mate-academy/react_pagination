87bbbad0ea035563957371d15cf4261c081f7c4d
newqou <137334905+newqou@users.noreply.github.com>
2023-10-31 13:14:50 +0200

    updated

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,10 +8,21 @@ interface Props {
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const moveToPreviousPage
-= (currentPage: number, setCurrentPage: React
-  .Dispatch<React.SetStateAction<number>>) => {
+const moveToPreviousPage = (
+  currentPage: number,
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>,
+) => {
   const newPage = currentPage - 1 <= 0 ? 1 : currentPage - 1;
+
+  setCurrentPage(newPage);
+};
+
+const moveToNextPage = (
+  currentPage: number,
+  countPage: number,
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>,
+) => {
+  const newPage = currentPage + 1 >= countPage ? countPage : currentPage + 1;
 
   setCurrentPage(newPage);
 };
@@ -23,13 +34,12 @@ export const Pagination: React.FC<Props> = ({
   setCurrentPage,
 }) => {
   const countPage = Math.ceil(items.length / countItemsPage);
+  const isCurrentPageDisabled = currentPage === countPage;
 
   return (
     <>
       <ul className="pagination">
-        <li
-          className={cn('page-item', { disabled: currentPage === 1 })}
-        >
+        <li className={cn('page-item', { disabled: currentPage === 1 })}>
           <a
             data-cy="prevLink"
             className="page-link"
@@ -40,38 +50,30 @@ export const Pagination: React.FC<Props> = ({
             «
           </a>
         </li>
-        {
-          [...Array(countPage)].map((_, i) => (
-            <li
-              className={cn('page-item', { active: i + 1 === currentPage })}
-              key={`#${i + 1}`}
+        {[...Array(countPage)].map((_, i) => (
+          <li
+            className={cn('page-item', { active: i + 1 === currentPage })}
+            key={`#${i + 1}`}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${i + 1}`}
+              onClick={() => setCurrentPage(i + 1)}
             >
-              <a
-                data-cy="pageLink"
-                className="page-link"
-                href={`#${i + 1}`}
-                onClick={() => setCurrentPage(i + 1)}
-              >
-                {i + 1}
-              </a>
-            </li>
-          ))
-        }
-        <li
-          className={cn('page-item', { disabled: currentPage === countPage })}
-        >
+              {i + 1}
+            </a>
+          </li>
+        ))}
+        <li className={cn('page-item', { disabled: isCurrentPageDisabled })}>
           <a
             data-cy="nextLink"
             className="page-link"
             href="#next"
-            aria-disabled={currentPage === countPage}
-            onClick={() => {
-              const newPage = currentPage + 1 >= countPage
-                ? countPage
-                : currentPage + 1;
-
-              setCurrentPage(newPage);
-            }}
+            aria-disabled={isCurrentPageDisabled}
+            onClick={
+              () => moveToNextPage(currentPage, countPage, setCurrentPage)
+            }
           >
             »
           </a>
@@ -79,19 +81,14 @@ export const Pagination: React.FC<Props> = ({
       </ul>
 
       <ul>
-        {
-          items.map((item, index) => (
-            ((currentPage - 1) * countItemsPage < index + 1
-              && currentPage * countItemsPage >= index + 1) && (
-              <li
-                data-cy="item"
-                key={item}
-              >
-                {item}
-              </li>
-            )
-          ))
-        }
+        {items.map((item, index) => (
+          (currentPage - 1) * countItemsPage < index + 1
+          && currentPage * countItemsPage >= index + 1 && (
+            <li data-cy="item" key={item}>
+              {item}
+            </li>
+          )
+        ))}
       </ul>
     </>
   );
