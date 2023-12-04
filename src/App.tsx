4f192de4200dafde92296008479ff9e8c8d1d0c4
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-12-04 20:23:52 +0200

    Revert "pagination"
    This reverts commit c49a10a80b435222ee20bbd33b3dfd69fdd25347.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { getNumbers } from './utils';
-import { Pagination } from './components/Pagination';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
-const totalPages = 42;
-const selectOptions = [3, 5, 10, 20];
-
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState(selectOptions[1]);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const maxItemPerPage = perPage * currentPage;
-  const firstItemOnPage = maxItemPerPage - perPage;
-
-  const lastItemOnPage = maxItemPerPage > totalPages
-    ? totalPages
-    : maxItemPerPage;
-
-  const onPageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setPerPage(+event.target.value);
-    setCurrentPage(1);
-  };
-
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        {`Page ${currentPage} (items ${firstItemOnPage + 1} - ${lastItemOnPage} of ${totalPages})`}
+        Page 1 (items 1 - 5 of 42)
       </p>
 
       <div className="form-group row">
@@ -44,15 +21,11 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
-            value={perPage}
-            onChange={handleChange}
           >
-
-            {selectOptions.map(option => (
-              <option value={option} key={option}>
-                {option}
-              </option>
-            ))}
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
           </select>
         </div>
 
@@ -61,18 +34,62 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      <Pagination
-        total={totalPages}
-        perPage={perPage}
-        currentPage={currentPage}
-        onPageChange={onPageChange}
-      />
+      {/* Move this markup to Pagination */}
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+          >
+            «
+          </a>
+        </li>
+        <li className="page-item active">
+          <a data-cy="pageLink" className="page-link" href="#1">1</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#2">2</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#3">3</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#4">4</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#5">5</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#6">6</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#7">7</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#8">8</a>
+        </li>
+        <li className="page-item">
+          <a data-cy="pageLink" className="page-link" href="#9">9</a>
+        </li>
+        <li className="page-item">
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+          >
+            »
+          </a>
+        </li>
+      </ul>
       <ul>
-        {items.slice(firstItemOnPage, lastItemOnPage).map(item => (
-          <li data-cy="item" key={item}>
-            {item}
-          </li>
-        ))}
+        <li data-cy="item">Item 1</li>
+        <li data-cy="item">Item 2</li>
+        <li data-cy="item">Item 3</li>
+        <li data-cy="item">Item 4</li>
+        <li data-cy="item">Item 5</li>
       </ul>
     </div>
   );
