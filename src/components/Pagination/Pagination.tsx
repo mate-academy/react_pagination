2619f3c15a1f413b50b109e124b8b4cbb02916e1
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-12-19 19:24:26 +0100

    Added full solution 2 errors in test with next and prev button

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,92 @@
-export const Pagination = () => {};
+import React from 'react';
+import classNames from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange?: (page: number) => void;
+  startItem: number;
+  endItem: number;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange = () => {},
+  startItem,
+  endItem,
+}) => {
+  const lastPage = Math.ceil(total / perPage);
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={classNames(
+          'page-item',
+          { disabled: currentPage === 1 },
+        )}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={() => onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+        {getNumbers(1, lastPage)
+          .map((n:number) => (
+            <li
+              className={classNames(
+                'page-item',
+                { active: currentPage === n },
+              )}
+              key={n}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`#${n}`}
+                onClick={() => {
+                  onPageChange(n);
+                }}
+              >
+                {n}
+              </a>
+            </li>
+          ))}
+        <li className={classNames(
+          'page-item',
+          { disabled: currentPage === lastPage },
+        )}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === lastPage}
+            onClick={() => onPageChange(currentPage + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {getNumbers(startItem, endItem)
+          .map((m:number) => (
+            <li
+              data-cy="item"
+              key={m}
+            >
+              {`Item ${m}`}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
