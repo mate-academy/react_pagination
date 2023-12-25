299dea9164538edbc9dab8d956a512b67e5077fc
Dmytro Torop <young-z@i.ua>
2023-12-25 16:52:25 +0200

    resolved issues causing test failures

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,36 @@ import './App.css';
 import { getNumbers } from './utils';
 import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+  .map(n => n);
+
+const selectOptions = [3, 5, 10, 20];
 
 export const App: React.FC = () => {
-  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [perPage, setPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const setPerPage = (event: ChangeEvent<HTMLSelectElement>): void => {
+  const setPerPageCount = (event: ChangeEvent<HTMLSelectElement>): void => {
     const newValue = +event.target.value;
 
-    setItemsPerPage(newValue);
+    setPerPage(newValue);
+    setCurrentPage(1);
+  };
+
+  const startItem = (currentPage - 1) * perPage;
+  const lastItem = (startItem + perPage) > items.length
+    ? items.length : startItem + perPage;
+
+  const visibleItems = [...items].slice(startItem, lastItem);
+
+  const setPage = (newPage: number) => {
+    if (
+      newPage >= 1
+      && newPage <= Math.ceil(items.length / perPage)
+      && newPage !== currentPage
+    ) {
+      setCurrentPage(newPage);
+    }
   };
 
   return (
@@ -21,7 +40,7 @@ export const App: React.FC = () => {
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${currentPage} (items ${startItem + 1} - ${lastItem} of ${items.length})`}
       </p>
 
       <div className="form-group row">
@@ -30,12 +49,12 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
-            onChange={setPerPage}
+            onChange={setPerPageCount}
+            value={perPage}
           >
-            <option value="3">3</option>
-            <option value="5" selected>5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            {
+              selectOptions.map(opt => <option value={opt}>{opt}</option>)
+            }
           </select>
         </div>
 
@@ -44,9 +63,17 @@ export const App: React.FC = () => {
         </label>
       </div>
       <Pagination
-        items={items}
-        itemsPerPage={itemsPerPage}
+        total={items.length}
+        perPage={perPage}
+        currentPage={currentPage}
+        onPageChange={setPage}
       />
+      <ul>
+        {
+          visibleItems
+            .map(item => <li key={item} data-cy="item">{`Item ${item}`}</li>)
+        }
+      </ul>
     </div>
   );
 };
