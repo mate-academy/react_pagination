661c1ea464b810771e8cb75d92fc0b717e05c457
Heorhii Churhuliia <gio4929189@gmail.com>
2024-01-06 00:01:18 +0000

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,88 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import React from 'react';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void,
+}
+
+export const Pagination: React.FC<Props> = ({
+  currentPage,
+  onPageChange,
+  total,
+  perPage,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const numberingPages = getNumbers(1, totalPages);
+
+  const firsPage = currentPage === 1;
+  const lastPage = currentPage === totalPages;
+
+  const moveNextPage = () => {
+    if (!lastPage) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  const movePrevPage = () => {
+    if (!firsPage) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const moveToCurrentPage = (page: number) => {
+    if (currentPage === page) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+
+      <li className={cn('page-item',
+        { disabled: firsPage })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled="true"
+          onClick={movePrevPage}
+        >
+          «
+        </a>
+      </li>
+
+      {numberingPages.map(i => (
+        <li
+          key={i}
+          className={cn('page-item',
+            { active: currentPage === i })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`${i}`}
+            onClick={() => moveToCurrentPage(i)}
+          >
+            {i}
+          </a>
+        </li>
+      ))}
+      <li className="page-item">
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled="false"
+          onClick={moveNextPage}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
