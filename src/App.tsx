d59f119410134997e2a079e03e48709194ee0376
Artem Velychko <avelychko@icloud.com>
2024-01-09 13:22:05 -0500

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,18 @@ const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState('5');
+  const [perPage, setPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const totalItems = items.length;
-  const totalPages = Math.ceil(totalItems / +perPage);
+  const totalPages = Math.ceil(totalItems / perPage);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
-  const itemsFrom = ((currentPage - 1) * +perPage) + 1;
+  const itemsFrom = ((currentPage - 1) * perPage) + 1;
   const itemsTo
-    = totalPages === currentPage ? totalItems : +perPage * currentPage;
+    = totalPages === currentPage ? totalItems : perPage * currentPage;
 
   const pageDescriprion = `Page ${currentPage} (items ${itemsFrom} - ${itemsTo} of ${totalItems})`;
 
@@ -38,7 +38,7 @@ export const App: React.FC = () => {
             className="form-control"
             value={perPage}
             onChange={(element) => {
-              setPerPage(element.target.value);
+              setPerPage(Number(element.target.value));
               setCurrentPage(1);
             }}
           >
@@ -56,7 +56,7 @@ export const App: React.FC = () => {
 
       <Pagination
         total={totalItems}
-        perPage={+perPage}
+        perPage={perPage}
         currentPage={currentPage}
         onPageChange={handlePageChange}
       />
