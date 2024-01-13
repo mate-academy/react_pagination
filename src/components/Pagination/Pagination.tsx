13fed179bbe9ed8b4d601db2b12e40e2db3e02cc
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-01-13 21:37:24 +0200

    react-pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,78 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { v4 as uuid } from 'uuid';
+
+type Props = {
+  total: number
+  perPage: number
+  currentPage: number
+  onPageChange: (page: number) => void
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+  function handleNextPage() {
+    if (currentPage < totalPages) {
+      onPageChange(currentPage + 1);
+    }
+  }
+
+  function handlePrevPage() {
+    if (currentPage !== 1) {
+      onPageChange(currentPage - 1);
+    }
+  }
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item', { disabled: currentPage === 1 })}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={handlePrevPage}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          className={cn('page-item', { active: currentPage === page })}
+          key={uuid()}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => {
+              if (currentPage !== page) {
+                onPageChange(page);
+              }
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li className={cn('page-item', { disabled: currentPage === totalPages })}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === totalPages}
+          onClick={handleNextPage}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
