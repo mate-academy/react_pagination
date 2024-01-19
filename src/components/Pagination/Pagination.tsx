6282cc2551fa4ca9b3cd263cf015b8e2fb6655e8
Yevhenii <polotnyak2004@gmail.com>
2024-01-19 02:00:30 +0200

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,100 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  itemsCount: number,
+  start: number,
+  end: number,
+  itemsPerPage: number,
+  currentPage: number,
+  onPageChange: (arg: number) => void,
+}
+
+export const Pagination = ({
+  itemsCount,
+  start,
+  end,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}: Props) => {
+  const pagesCount = Math.ceil(itemsCount / itemsPerPage);
+  const pages = getNumbers(1, pagesCount);
+
+  const getItemsForPage = () => {
+    return getNumbers(start, end).map(n => `Item ${n}`);
+  };
+
+  const handlePageChange = (page: number) => {
+    if (page !== currentPage && page >= 1 && page <= pagesCount) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={cn(
+            'page-item',
+            { disabled: currentPage === 1 },
+          )}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 && 'true'}
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+
+        {pages.map(page => (
+          <li
+            key={page}
+            className={cn(
+              'page-item',
+              { active: currentPage === page },
+            )}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${page}`}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={cn(
+            'page-item',
+            { disabled: currentPage === pagesCount },
+          )}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === pagesCount && 'true'}
+            onClick={() => handlePageChange(currentPage + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        {getItemsForPage().map(item => (
+          <li key={item} data-cy="item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
