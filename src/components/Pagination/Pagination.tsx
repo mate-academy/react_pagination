f5155b0757703d80bbd4cf14990f6db8655a70f2
Mariana <marjanavv@gmail.com>
2024-01-24 10:02:03 +0100

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,88 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import { getNumbers } from '../../utils';
+
+
+interface Paginations {
+  total: number
+  currentPage: number
+  perPage: number
+  onPageChange: (page: number) => void
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+export const Pagination
+  = ({
+    total, currentPage, perPage, onPageChange,
+  }: Paginations) => {
+    function onPrevPage() {
+      return currentPage !== 1 && onPageChange(currentPage - 1);
+    }
+
+    function onNextPage() {
+      return currentPage !== Math.ceil(total / (perPage))
+      && onPageChange(currentPage + 1);
+    }
+
+    return (
+      <ul className="pagination">
+        <li className={classNames('page-item',
+          { disabled: currentPage === 1 })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 ? 'true' : 'false'}
+            onClick={() => {
+              onPrevPage();
+            }}
+          >
+            «
+          </a>
+        </li>
+
+
+
+
+        {getNumbers(1, Math.ceil(total / (perPage)))
+          .map(item => (
+            <li
+              className={classNames('page-item',
+                { active: currentPage === item })}
+              key={1}
+            >
+
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href="#"
+                onClick={() => {
+                  onPageChange(item);
+                }}
+              >
+                {item}
+              </a>
+
+            </li>
+          ))}
+
+        <li className={classNames('page-item',
+          { disabled: currentPage === Math.ceil(total / (perPage)) })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === Math.ceil(total / (perPage))
+              ? 'true' : 'false'}
+            onClick={() => {
+              onNextPage();
+            }}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    );
+  };
