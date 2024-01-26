bde61ce4fe3a119011ce3539ac0374939c5c5f8d
Mykhailo <mykhailoevo@gmail.com>
2024-01-26 14:54:29 +0200

    move functions outside

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,35 @@ import { Pagination } from './components/Pagination';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const items = getNumbers(1, 42).map((n) => `Item ${n}`);
 
+const getPaginationList = (item: string[], itemsPerPage: number): number[] => {
+  const pagin = [];
+
+  for (let i = 1; i <= Math.ceil(item.length / itemsPerPage); i += 1) {
+    pagin.push(i);
+  }
+
+  return pagin;
+};
+
+const tileCount = (page: number, itemsPerPage: number) => {
+  const firstItemOnPage = itemsPerPage * page - itemsPerPage + 1;
+  const lastItemOnPage = itemsPerPage * page < 42 ? itemsPerPage * page : 42;
+
+  return `Page ${page} (items ${firstItemOnPage} - ${lastItemOnPage} of 42)`;
+};
+
 export const App: React.FC = () => {
   const [page, setPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
 
-  const pagination = () => {
-    const pagin = [];
-
-    for (let i = 1; i <= Math.ceil(items.length / itemsPerPage); i += 1) {
-      pagin.push(i);
-    }
-
-    return pagin;
-  };
-
-  const paginationList = pagination();
-
-  const tile = () => {
-    const firstItemOnPage = itemsPerPage * page - itemsPerPage + 1;
-    const lastItemOnPage = itemsPerPage * page < 42 ? itemsPerPage * page : 42;
-
-    return `Page ${page} (items ${firstItemOnPage} - ${lastItemOnPage} of 42)`;
-  };
+  const paginationList = getPaginationList(items, itemsPerPage);
 
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        {tile()}
+        {tileCount(page, itemsPerPage)}
       </p>
 
       <div className="form-group row">
