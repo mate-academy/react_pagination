f47d90c3166d0aed559aea4c81797a36f128dbd9
Evhen Holub <golub0208@gmail.com>
2024-01-29 17:04:45 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42)
-  .map(n => `Item ${n}`);
+const total = 42;
+const options = [3, 5, 10, 20];
+const items = getNumbers(1, total)
+  .map(n => n);
 
 export const App: React.FC = () => {
+  const [perPage, setPerPage] = useState(options[1]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const perPageSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPerPage(+event.target.value); // встановлюємо кількість елементів на сторінці
+    setCurrentPage(1); // скидаємо на першу сторінку (за замовчуванням)
+  };
+
+  function getNumberOfPage() {
+    return items.filter(number => {
+      return +number > (currentPage * perPage) - perPage
+        && +number <= currentPage * perPage;
+    });
+  }
+
+  const pageItems = getNumberOfPage(); // створили масив з кількістю елементів сторінки
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${currentPage} (items ${pageItems[0]} - ${pageItems[pageItems.length - 1]} of ${total})`}
       </p>
 
       <div className="form-group row">
@@ -21,11 +41,14 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            value={perPage}
+            onChange={perPageSelect}
           >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            {options.map((opt) => ( // оптимізуємо значення для вибору кількості елементів на сторінці
+              <option key={opt} value={opt}>
+                {opt}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -34,62 +57,19 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={total}
+        perPage={perPage}
+        currentPage={currentPage}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {pageItems.map(item => ( // оптимізуємо елемети сторінки
+          <li data-cy="item" key={item}>
+            {`Item ${item}`}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,71 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (page: number) => void,
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const countPages = Math.ceil(total / perPage); // рахуємо кількість сторінок
+  const pageButtons = getNumbers(1, countPages); // створюємо масив з відповідною кількістю сторінок
+  const changePage = (buttonNumber: number) => { // оптимізуємо зміну сторінки
+    if (buttonNumber >= 1
+      && buttonNumber <= pageButtons.length
+      && buttonNumber !== currentPage) {
+      onPageChange(buttonNumber);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item', { disabled: currentPage === 1 })}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={() => changePage(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+
+      {pageButtons.map((buttonNumber) => (
+        <li
+          className={cn('page-item', { active: buttonNumber === currentPage })}
+          key={buttonNumber}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${buttonNumber}`}
+            onClick={() => changePage(buttonNumber)}
+          >
+            {buttonNumber}
+          </a>
+        </li>
+      ))}
+
+      <li className={cn('page-item', { disabled: currentPage === countPages })}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === countPages}
+          onClick={() => changePage(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
