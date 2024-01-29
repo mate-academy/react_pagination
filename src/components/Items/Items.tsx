92061a512fe1e3e7d67a925b25c90df9922f6796
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-01-29 20:46:16 +0200

    solution Pagination

---
+++ src/components/Items/Items.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type Props = {
+  itemsList: string[];
+};
+
+export const Items: React.FC<Props> = ({ itemsList }) => (
+  <ul>
+    {itemsList.map((item: string) => (
+      <li
+        data-cy="item"
+        key={item}
+      >
+        {item}
+      </li>
+    ))}
+  </ul>
+);
