f8092ab21a95a23ea94aebb0c6616f024f9789ea
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2024-02-19 22:22:32 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+type handleChange = (newPage: number, newPerPage: number) => void
+
 const items = getNumbers(1, 42)
   .map(n => `Item ${n}`);
 
+
 export const App: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const itemQuantity = [3, 5, 10, 20];
+  const [perPage, setPerPage] = useState(itemQuantity[1]);
+
+  const totalPages = Math.ceil(items.length / perPage);
+
+
+  const handleChange: handleChange = (newPage, newPerPage) => {
+    setPage(newPage);
+    setPerPage(newPerPage);
+  }
+
+  useEffect(() => {
+    const newParams = new URLSearchParams();
+    newParams.set('page', String(page));
+    newParams.set('perPage', String(perPage));
+    window.history.replaceState({}, '', `${location.pathname}?${newParams}`);
+  }, [page, perPage, location.pathname]);
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        {`Page ${page} (items ${(perPage * page) - perPage + 1} - ${Math.min(perPage * page, items.length)} of ${items.length})`}
       </p>
 
       <div className="form-group row">
@@ -21,11 +45,10 @@ export const App: React.FC = () => {
             data-cy="perPageSelector"
             id="perPageSelector"
             className="form-control"
+            onChange={(e) => (handleChange(1, Number(e.target.value)))}
+            value={perPage}
           >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            {itemQuantity.map(option => <option key={option} value={option}>{option}</option>)}
           </select>
         </div>
 
@@ -34,64 +57,18 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true"
-          >
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">1</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">2</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">3</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">4</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">5</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">6</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">7</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">8</a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">9</a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false"
-          >
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={totalPages}
+        currentPage={page}
+        perPage={perPage}
+        onPageChange={handleChange}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {items.slice((page - 1) * perPage, page * perPage).map((item, index) => <li key={index} data-cy="item">{item}</li>)}
       </ul>
     </div>
+
   );
 };
 
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,50 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+
+
+interface Props {
+  total: number;
+  currentPage: number;
+  perPage: number;
+  onPageChange: (newPage: number, newPerPage: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({ total, currentPage, perPage, onPageChange }) => {
+  return (
+    <ul className="pagination">
+        <li className={cn('page-item', {'disabled': currentPage === 1})}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={() => currentPage > 1 && onPageChange( currentPage - 1, perPage)}
+          >
+            «
+          </a>
+        </li>
+        {Array.from({ length: total }).map((_, index) => (
+        <li key={index + 1} className={cn('page-item', {active: currentPage === index + 1})}>
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${index + 1}`}
+            onClick={() => onPageChange(index + 1, perPage)}
+          >
+            {index + 1}
+          </a>
+        </li>))}
+        <li className={cn('page-item', {disabled: currentPage === total})}>
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === total}
+            onClick={() => currentPage < total && onPageChange(currentPage + 1, perPage)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+  );
+};
