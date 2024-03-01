de7572fe8f9a1b69ad185b6f24b537e655dc651c
volodymyr <volodiayurovych@gmail.com>
2024-03-01 18:28:27 +0100

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { getNumbers } from './utils';
 import { Pagination } from './components/Pagination';
 
 export const App: React.FC = () => {
-  const [perPage, setPerPage] = useState<number>(5);
+  const [perPage, setPerPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
 
   const selectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -18,51 +18,53 @@ export const App: React.FC = () => {
   const whatToShow = getNumbers(fromHere, toHere).map(n => `Item ${n}`);
 
   return (
-    <div className="container">
-      <h1>Items with Pagination</h1>
+    <>
+      <div className="container">
+        <h1>Items with Pagination</h1>
 
-      <p className="lead" data-cy="info">
-        {`Page ${currentPage} (items ${fromHere} - ${toHere} of 42)`}
-      </p>
+        <p className="lead" data-cy="info">
+          {`Page ${currentPage} (items ${fromHere} - ${toHere} of 42)`}
+        </p>
 
-      <div className="form-group row">
-        <div className="col-3 col-sm-2 col-xl-1">
-          <select
-            data-cy="perPageSelector"
-            id="perPageSelector"
-            className="form-control"
-            value={perPage}
-            onChange={selectChange}
-          >
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-          </select>
+        <div className="form-group row">
+          <div className="col-3 col-sm-2 col-xl-1">
+            <select
+              data-cy="perPageSelector"
+              id="perPageSelector"
+              className="form-control"
+              value={perPage}
+              onChange={selectChange}
+            >
+              <option value="3">3</option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+            </select>
+          </div>
+
+          <label htmlFor="perPageSelector" className="col-form-label col">
+            items per page
+          </label>
         </div>
 
-        <label htmlFor="perPageSelector" className="col-form-label col">
-          items per page
-        </label>
+        <Pagination
+          total={42}
+          perPage={perPage}
+          currentPage={currentPage}
+          onPageChange={page => {
+            setCurrentPage(page);
+          }}
+        />
+
+        <ul>
+          {whatToShow.map(item => (
+            <li data-cy="item" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <Pagination
-        total={42}
-        perPage={perPage}
-        currentPage={currentPage}
-        onPageChange={page => {
-          setCurrentPage(page);
-        }}
-      />
-
-      <ul>
-        {whatToShow.map(item => (
-          <li data-cy="item" key={item}>
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 };
 
