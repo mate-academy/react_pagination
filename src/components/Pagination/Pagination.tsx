4dbb4e6108d77cd3ae3e7e4ae6b73ffba3d6e88d
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-03-12 16:40:26 +0200

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,75 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import { PAGES_START_INDEX, PAGES_STEP } from '../../constants';
+
+interface Props {
+  numbersOfPages: number[];
+  selectedPage: number;
+  onPageChange: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export const Pagination: React.FC<Props> = ({
+  numbersOfPages,
+  selectedPage,
+  onPageChange,
+}) => {
+  return (
+    <ul className="pagination">
+      <li
+        className={classNames('page-item', {
+          disabled: selectedPage === PAGES_START_INDEX,
+        })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={selectedPage === PAGES_START_INDEX}
+          onClick={() => {
+            if (selectedPage <= numbersOfPages.length) {
+              onPageChange(selectedPage - PAGES_STEP);
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {numbersOfPages.map(page => (
+        <li
+          className={classNames('page-item', {
+            active: selectedPage === page,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={classNames('page-item', {
+          disabled: selectedPage === numbersOfPages.length,
+        })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={selectedPage === numbersOfPages.length}
+          onClick={() => {
+            if (selectedPage < numbersOfPages.length) {
+              onPageChange(selectedPage + PAGES_STEP);
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
