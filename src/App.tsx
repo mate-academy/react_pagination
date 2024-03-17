c625434b75370162fc6a6cb2880514c0107a5d36
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-03-17 15:21:49 +0100

    Add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,10 @@ export const App: React.FC = () => {
 
   const maxPage = Math.ceil(items.length / +perPageItems);
 
-  const title = selectedPage === 1 ? `Page ${selectedPage} (items 1 - ${+perPageItems * selectedPage} of 42)` : `Page ${selectedPage} (items ${((selectedPage - 1) * +perPageItems) + 1} - ${Math.min(+perPageItems * selectedPage, items.length)} of ${items.length})`;
+  const title =
+    selectedPage === 1
+      ? `Page ${selectedPage} (items 1 - ${+perPageItems * selectedPage} of 42)`
+      : `Page ${selectedPage} (items ${(selectedPage - 1) * +perPageItems + 1} - ${Math.min(+perPageItems * selectedPage, items.length)} of ${items.length})`;
 
   return (
     <div className="container">
@@ -50,7 +53,6 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
       <Pagination
         items={items}
         perPageItems={perPageItems}
@@ -58,78 +60,6 @@ export const App: React.FC = () => {
         maxPage={maxPage}
         handlerSelectedPage={handlerSelectedPage}
       />
-      {/* <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true">
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">
-            5
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">
-            6
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">
-            7
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">
-            8
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">
-            9
-          </a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false">
-            »
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
-      </ul> */}
     </div>
   );
 };
