e151a3deb86eaebfcbee44ee3d7dec02f9b362ba
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-03-18 18:43:19 +0100

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<App />);
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
