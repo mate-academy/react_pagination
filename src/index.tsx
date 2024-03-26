2a3d7d4be9cd1c68f7a6cd2ca7ba3f2f6cfa1812
Victoria Pukalo <pukalovictoria@gmail.com>
2024-03-26 19:22:25 +0100

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const domNode = document.getElementById('root')!;
+
+const root = createRoot(domNode);
+
+root.render(<App />);
