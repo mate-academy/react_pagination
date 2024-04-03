140b903b1ce640bba7823c34d40690067878a3b5
Hlib Kaponkin <glebkapenkin@gmail.com>
2024-04-03 22:38:01 +0300

    sol

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,71 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+
+type Props = {
+  items: string[];
+  currentPage: number;
+  setCurrentPage: (pageNumber: number) => void;
+  perPage: number;
+  onPageChange: string[];
+};
+
+export const Pagination: React.FC<Props> = ({
+  items,
+  currentPage,
+  setCurrentPage,
+  perPage,
+  onPageChange,
+}) => (
+  <>
+    <ul className="pagination">
+      <li className={cn({ disabled: currentPage === 1 }, 'page-item')}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+      {Array.from({ length: Math.ceil(items.length / perPage) }, (_, index) => (
+        <li
+          className={cn({ active: currentPage === index + 1 }, 'page-item')}
+          key={index}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${index + 1}`}
+            onClick={() => setCurrentPage(index + 1)}
+          >
+            {index + 1}
+          </a>
+        </li>
+      ))}
+      <li
+        className={cn(
+          { disabled: currentPage === Math.ceil(items.length / perPage) },
+          'page-item',
+        )}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === Math.ceil(items.length / perPage)}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+    <ul>
+      {onPageChange.map(item => (
+        <li data-cy="item" key={item}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  </>
+);
