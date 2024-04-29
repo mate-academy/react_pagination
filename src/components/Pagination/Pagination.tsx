1dcac9a1792c1ad5d56a2d883381b7306cb11dfb
Valentyn Sobol <valiksobol14@gmail.com>
2024-04-29 16:00:26 +0300

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,77 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const optPages = Math.ceil(total / perPage);
+  const renderPages = getNumbers(1, optPages);
+
+  return (
+    <ul className="pagination">
+      <li
+        className={cn({
+          'page-item': true,
+          disabled: currentPage === 1,
+        })}
+        onClick={
+          currentPage !== 1 ? () => onPageChange(currentPage - 1) : () => {}
+        }
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled="true"
+        >
+          «
+        </a>
+      </li>
+      {renderPages.map(page => (
+        <li
+          className={cn({
+            'page-item': true,
+            active: currentPage === page,
+          })}
+          key={page}
+          onClick={() => onPageChange(page)}
+        >
+          <a data-cy="pageLink" className="page-link" href={`#${page}`}>
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={cn({
+          'page-item': true,
+          disabled: currentPage === optPages,
+        })}
+        onClick={
+          currentPage !== optPages
+            ? () => onPageChange(currentPage + 1)
+            : () => {}
+        }
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === optPages}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
