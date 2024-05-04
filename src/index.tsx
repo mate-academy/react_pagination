8a2f893256149ae339323e1ad7a6516eeeabb3ad
Vladislav <7evenLays@gmail.com>
2024-05-04 12:35:20 +0300

    removed router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+// ]);
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={router} />,
-);
+// createRoot(document.getElementById('root') as HTMLElement).render(
+//   <RouterProvider router={router} />,
+// );
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
