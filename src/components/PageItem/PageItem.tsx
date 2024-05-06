9edbd6d92adba40b74f2d2344207c6c453f47c5c
Mykhailo Lapchynskyi <mykhailolapchynskyi@Mykhailos-MacBook-Air.local>
2024-05-06 17:00:03 +0200

    task solution

---
+++ src/components/PageItem/PageItem.tsx
@@ -0,0 +1,3 @@
+// export const PageItem = () => {
+//   return ()
+// }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,78 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+
+import { PaginationProps } from '../../types/PaginationProps';
+
+export const Pagination = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}: PaginationProps) => {
+  const pageNumber = Math.ceil(total / perPage);
+  const generatePageItems = (pageAmount: number) => {
+    const pages = [];
+
+    for (let i = 1; i < pageAmount + 1; i++) {
+      pages.push(
+        <li
+          className={cn('page-item', {
+            active: currentPage === i,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${i}`}
+            onClick={() => {
+              onPageChange(i);
+            }}
+          >
+            {i}
+          </a>
+        </li>,
+      );
+    }
+
+    return pages;
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={cn('page-item', {
+            disabled: currentPage === 1,
+          })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 ? 'true' : 'false'}
+            onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+        {generatePageItems(pageNumber)}
+        <li
+          className={cn('page-item', {
+            disabled: currentPage === pageNumber,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === pageNumber ? 'true' : 'false'}
+            onClick={() =>
+              currentPage < pageNumber && onPageChange(currentPage + 1)
+            }
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
