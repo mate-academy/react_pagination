653f375154f36af7105343a9bc265d2d37a0d797
Vladislav <7evenLays@gmail.com>
2024-05-08 12:49:26 +0300

    fixed demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,3 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//   },
-// ]);
-
-// createRoot(document.getElementById('root') as HTMLElement).render(
-//   <RouterProvider router={router} />,
-// );
 createRoot(document.getElementById('root') as HTMLElement).render(<App />);
