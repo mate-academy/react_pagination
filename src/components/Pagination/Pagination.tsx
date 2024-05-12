12f34f2c3d50f9108f779accb37cbcb4bf987205
Erik Hulinian <gulinianerik2@gmail.com>
2024-05-12 14:18:34 +0300

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,43 @@
-export const Pagination = () => {};
+import React from 'react';
+import {getPages} from "../../utils";
+
+interface PaginationProps {
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ currentPage, onPageChange }) => {
+  const pages = getPages(1, 9).map(i => `${i}`);
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <button
+          className="page-link"
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          «
+        </button>
+      </li>
+      {pages.map(page => (
+        <li key={page} className={`page-item ${parseInt(page) === currentPage ? 'active' : ''}`}>
+          <button className="page-link" onClick={() => onPageChange(parseInt(page))}>
+            {page}
+          </button>
+        </li>
+      ))}
+      <li className={`page-item ${currentPage === pages.length ? 'disabled' : ''}`}>
+      <button
+          className="page-link"
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === pages.length}
+        >
+          »
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
