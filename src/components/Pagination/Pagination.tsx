f6552706ec5e29a4829cd8262f387561014edbb2
Vadym Babisov <vadim.babisov@gmail.com>
2024-07-29 17:22:32 +0300

    add solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,85 @@
-export const Pagination = () => {};
+import React from 'react';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const numberOfPages = Math.ceil(total / perPage);
+  const pages = Array.from({ length: numberOfPages }, (_, i) => i + 1);
+
+  const handlePrevClick = (
+    currentEvent: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    currentEvent.preventDefault();
+    if (currentPage > 1) {
+      onPageChange(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = (
+    currentEvent: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    currentEvent.preventDefault();
+    if (currentPage < numberOfPages) {
+      onPageChange(currentPage + 1);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1 ? 'true' : 'false'}
+          onClick={handlePrevClick}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          className={`page-item ${page === currentPage ? 'active' : ''}`}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={e => {
+              e.preventDefault();
+              onPageChange(page);
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li
+        className={`page-item ${currentPage === numberOfPages ? 'disabled' : ''}`}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === numberOfPages ? 'true' : 'false'}
+          onClick={handleNextClick}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
