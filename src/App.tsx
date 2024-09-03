423d3fdd96b67873bf4f1d9464b8fae71ec3d0c2
Seva Podolskiy <s_podolskiy@live.com>
2024-09-03 12:14:23 -0400

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
+import { Pagination } from './components/Pagination';
+import { PaginationInfo } from './components/PaginationInfo';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const items = getNumbers(1, 42).map(n => `Item ${n}`);
+const items: number[] = getNumbers(1, 42).map(n => n);
+
+const perPageOptions = [3, 5, 10, 20].map((el, i) => {
+  return { id: i, val: el };
+});
+
+interface PageItem {
+  id: number;
+  val: number;
+}
 
 export const App: React.FC = () => {
+  function buildPagesContainer(
+    itms: number[],
+    chunkSize: number,
+  ): PageItem[][] {
+    const contArr: PageItem[][] = [];
+    let globId = 0;
+
+    if (!chunkSize) {
+      return [];
+    }
+
+    for (let i = 0; i < itms.length; i += chunkSize) {
+      const chunk = itms.slice(i, i + chunkSize);
+      const subCont = [];
+
+      for (let i2 = 0; i2 < chunk.length; i2++) {
+        subCont.push({ id: globId, val: chunk[i2] });
+        globId++;
+      }
+
+      contArr.push(subCont);
+    }
+
+    return contArr;
+  }
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(perPageOptions[1].val);
+  const pagesContainer = buildPagesContainer(items, perPage);
+  const currentPageContainer = pagesContainer[currentPage - 1];
+
+  function handlePageSelection(pageNum: number): void {
+    if (pageNum && pageNum <= pagesContainer.length) {
+      setCurrentPage(pageNum);
+    }
+  }
+
+  function handlePagePerSelection(val: any) {
+    setCurrentPage(1);
+    setPerPage(parseInt(val.target.value));
+  }
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
-      <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
-      </p>
+      <PaginationInfo
+        total={items.length}
+        currentPage={currentPage}
+        firstItem={currentPageContainer[0].val}
+        lastItem={currentPageContainer[currentPageContainer.length - 1].val}
+      />
 
       <div className="form-group row">
         <div className="col-3 col-sm-2 col-xl-1">
           <select
             data-cy="perPageSelector"
             id="perPageSelector"
-            className="form-control">
-            <option value="3">3</option>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
+            className="form-control"
+            onChange={handlePagePerSelection}
+            defaultValue={5}
+          >
+            {perPageOptions.map(p => (
+              <option key={p.id} value={`${p.val}`}>
+                {p.val}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -32,78 +92,21 @@ export const App: React.FC = () => {
         </label>
       </div>
 
-      {/* Move this markup to Pagination */}
-      <ul className="pagination">
-        <li className="page-item disabled">
-          <a
-            data-cy="prevLink"
-            className="page-link"
-            href="#prev"
-            aria-disabled="true">
-            «
-          </a>
-        </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">
-            5
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">
-            6
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">
-            7
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">
-            8
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">
-            9
-          </a>
-        </li>
-        <li className="page-item">
-          <a
-            data-cy="nextLink"
-            className="page-link"
-            href="#next"
-            aria-disabled="false">
-            »
-          </a>
-        </li>
-      </ul>
+      <Pagination
+        total={items.length}
+        perPage={perPage}
+        currentPage={currentPage}
+        onPageChange={handlePageSelection}
+      />
+
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {currentPageContainer.map(pg => {
+          return (
+            <li key={pg.id} data-cy="item">
+              {`Item ${pg.val}`}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
