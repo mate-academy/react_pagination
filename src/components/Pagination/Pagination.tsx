ba8bf995435af7c4dc5fb9c132a308165ba66263
Oleksandr Spyrydonov <spiridonovsasa731@gmail.com>
2024-09-23 16:49:30 +0300

    solution of task react-pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,86 @@
-export const Pagination = () => {};
+import React from 'react';
+import { getNumbers } from '../../utils';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (newPage: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const linksAmount = Math.ceil(total / perPage);
+  const linkNumbers = getNumbers(1, linksAmount);
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1 ? 'true' : 'false'}
+          onClick={(event: React.MouseEvent<HTMLAnchorElement>) => {
+            event.preventDefault();
+
+            const newPage = currentPage - 1;
+
+            if (newPage >= 1) {
+              onPageChange(newPage);
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {linkNumbers.map(linkNumber => {
+        return (
+          <li
+            className={`page-item ${currentPage === linkNumber ? 'active' : ''}`}
+            key={linkNumber}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${linkNumber}`}
+              onClick={(event: React.MouseEvent<HTMLAnchorElement>) => {
+                event.preventDefault();
+                onPageChange(linkNumber);
+              }}
+            >
+              {linkNumber}
+            </a>
+          </li>
+        );
+      })}
+
+      <li
+        className={`page-item ${currentPage === linksAmount ? 'disabled' : ''}`}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === linksAmount ? 'true' : 'false'}
+          onClick={(event: React.MouseEvent<HTMLAnchorElement>) => {
+            event.preventDefault();
+
+            const newPage = currentPage + 1;
+
+            if (newPage <= linksAmount) {
+              onPageChange(newPage);
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
