7c4c685c3d9cc59dd4cfac2f773aa92b50eb533e
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-10-01 09:38:05 +0200

    refactor: render pages with map method in Pagination comp

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,16 +2,62 @@ import React from 'react';
 
 type PaginationProps = {
   total: string[];
+  perPage: number;
+  currentPage: number;
 };
 
-export const Pagination = ({ total }: PaginationProps) => {
+export const Pagination = ({
+  total,
+  perPage,
+  currentPage,
+}: PaginationProps) => {
+  const totalPages = Math.ceil(total.length / perPage);
+
   return (
-    <ul>
-      {total.map(item => (
-        <li key={item} data-cy="item">
-          {item}
+    <>
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+          >
+            «
+          </a>
         </li>
-      ))}
-    </ul>
+        {[...Array(totalPages)].map((_, index) => {
+          const page = index + 1;
+
+          return (
+            <li
+              key={page}
+              className={`page-item ${page === currentPage ? 'active' : ''}`}
+            >
+              <a data-cy="pageLink" className="page-link" href={`#${page}`}>
+                {page}
+              </a>
+            </li>
+          );
+        })}
+        <li className="page-item">
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {total.map(item => (
+          <li key={item} data-cy="item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
