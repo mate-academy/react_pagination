1aafc8a068d6a76a4eed947d938d182d8c6baffc
Daria Nastas <bycinska@gmail.com>
2024-10-01 13:40:59 +0300

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,83 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+import React from 'react';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+
+  const handlePageChange = (page: number) => {
+    if (page !== currentPage && page >= 1 && page <= totalPages) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    <ul className="pagination">
+      <li className={classNames('page-item', { disabled: currentPage === 1 })}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={e => {
+            e.preventDefault();
+            handlePageChange(currentPage - 1);
+          }}
+        >
+          «
+        </a>
+      </li>
+
+      {Array.from({ length: totalPages }, (_, index) => (
+        <li
+          key={index + 1}
+          className={classNames('page-item', {
+            active: currentPage === index + 1,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${index + 1}`}
+            onClick={e => {
+              e.preventDefault();
+              handlePageChange(index + 1);
+            }}
+          >
+            {index + 1}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={classNames('page-item', {
+          disabled: currentPage === totalPages,
+        })}
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === totalPages}
+          onClick={e => {
+            e.preventDefault();
+            handlePageChange(currentPage + 1);
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
