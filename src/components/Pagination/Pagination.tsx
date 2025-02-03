d652391b5b9c7be5e98fc51fc34ded72b1ba979f
Velislav Georgiev <velislav.georgiev89@gmail.com>
2025-02-03 20:05:10 +0200

    solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,79 @@
-export const Pagination = () => {};
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link } from 'react-router-dom';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        <Link
+          data-cy="prevLink"
+          className="page-link"
+          to={`?page=${currentPage - 1}&perPage=${perPage}`}
+          aria-disabled={currentPage === 1}
+          onClick={e => {
+            // eslint-disable-next-line brace-style
+            if (currentPage === 1) {
+              e.preventDefault();
+              // eslint-disable-next-line prettier/prettier, brace-style
+            } else {
+              onPageChange(currentPage - 1);
+            }
+          }}
+        >
+          «
+        </Link>
+      </li>
+
+      {pageNumbers.map(page => (
+        <li
+          key={page}
+          className={`page-item ${page === currentPage ? 'active' : ''}`}
+        >
+          <Link
+            data-cy="pageLink"
+            className="page-link"
+            to={`?page=${page}&perPage=${perPage}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </Link>
+        </li>
+      ))}
+
+      <li
+        className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+      >
+        <Link
+          data-cy="nextLink"
+          className="page-link"
+          to={`?page=${currentPage + 1}&perPage=${perPage}`}
+          aria-disabled={currentPage === totalPages}
+          onClick={e => {
+            // eslint-disable-next-line curly
+            if (currentPage === totalPages) e.preventDefault();
+            // eslint-disable-next-line curly
+            else onPageChange(currentPage + 1);
+          }}
+        >
+          »
+        </Link>
+      </li>
+    </ul>
+  );
+};
