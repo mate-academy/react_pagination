a9639bfa5b9f6b4b3ea9bcc22aa1c08097036b2a
prutasveta <svetapruta@gmail.com>
2025-02-07 11:08:30 +0100

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,77 @@
-export const Pagination = () => {};
+import classNames from 'classnames';
+
+import { getNumbers } from '../../utils';
+import React from 'react';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pageItems = getNumbers(1, Math.ceil(total / perPage));
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={classNames('page-item', {
+            disabled: currentPage === 1,
+          })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 ? 'true' : 'false'}
+            onClick={() => onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+        {pageItems.map(pageItem => {
+          return (
+            <li
+              className={classNames('page-item', {
+                active: pageItem === currentPage,
+              })}
+              key={pageItem}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`#${pageItem}`}
+                onClick={() => onPageChange(pageItem)}
+              >
+                {pageItem}
+              </a>
+            </li>
+          );
+        })}
+
+        <li
+          className={classNames('page-item', {
+            disabled: currentPage === pageItems.length,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === pageItems.length ? 'true' : 'false'}
+            onClick={() => onPageChange(currentPage + 1)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
