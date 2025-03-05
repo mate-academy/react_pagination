33b780f94c08cec9c7df9155e1220b9f54c3eef4
Lirik <melnicukkiril2002@gmail.com>
2025-03-05 22:42:21 +0200

    add task solution

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,9 @@ export function getNumbers(from: number, to: number): number[] {
 
   return numbers;
 }
+
+export function getPages(itemsPerPage: number, totalItems: number): number[] {
+  const pages = getNumbers(1, Math.ceil(totalItems / itemsPerPage));
+
+  return pages;
+}
