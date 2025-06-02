10f9f002e2ce042eb7dc6c2c1e53f9dea31b1096
user <mariiaantoniv@gmail.com>
2025-06-03 00:11:33 +0300

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,66 @@
-export const Pagination = () => {};
+import React from 'react';
+
+interface PropsPaginator {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<PropsPaginator> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const prevDisabled = currentPage === 1;
+  const nextDiable = currentPage === totalPages;
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item  ${prevDisabled ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={prevDisabled}
+          onClick={() => !prevDisabled && onPageChange(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+      {[...Array(totalPages)].map((_, i) => {
+        const index = i + 1;
+
+        return (
+          <li
+            key={index}
+            className={`page-item ${index === currentPage ? 'active' : ''}`}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${index}`}
+              onClick={() => onPageChange(index)}
+            >
+              {index}
+            </a>
+          </li>
+        );
+      })}
+
+      <li className={`page-item ${nextDiable ? 'disabled' : ''}`}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={nextDiable}
+          onClick={() => !nextDiable && onPageChange(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
