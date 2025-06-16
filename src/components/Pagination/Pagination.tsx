a4081f51473f1598c32381855f2db731159f5ed0
Jenya Golovko <u9on1210@gmail.com>
2025-06-16 15:51:57 +0300

    task has been done

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,109 @@
-export const Pagination = () => {};
+import React from 'react';
+import { PaginationProps } from '../../types/PaginationProps';
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+  onPerPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pages: number[] = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  const handlePageChange = (page: number) => {
+    if (page !== currentPage) {
+      onPageChange(page);
+    }
+  };
+
+  const handlePerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newPerPage = parseInt(e.target.value, 10);
+
+    onPerPageChange(newPerPage);
+  };
+
+  const firstItemIndex = (currentPage - 1) * perPage + 1;
+  const lastItemIndex = Math.min(currentPage * perPage, total);
+
+  return (
+    <div>
+      <p data-cy="info">
+        Page {currentPage} (items {firstItemIndex} - {lastItemIndex} of {total})
+      </p>
+      <div className="form-group row">
+        <div className="col-3 col-sm-2 col-xl-1">
+          <select
+            data-cy="perPageSelector"
+            id="perPageSelector"
+            className="form-control"
+            value={perPage}
+            onChange={handlePerPageChange}
+          >
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+        <label htmlFor="perPageSelector" className="col-form-label col">
+          items per page
+        </label>
+      </div>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={e => {
+              e.preventDefault();
+              if (currentPage > 1) {
+                onPageChange(currentPage - 1);
+              }
+            }}
+          >
+            «
+          </a>
+        </li>
+        {pages.map(page => (
+          <li
+            key={page}
+            className={`page-item ${page === currentPage ? 'active' : ''}`}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={`#${page}`}
+              onClick={e => {
+                e.preventDefault();
+                handlePageChange(page);
+              }}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+        <li
+          className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === totalPages}
+            onClick={e => {
+              e.preventDefault();
+              if (currentPage < totalPages) {
+                onPageChange(currentPage + 1);
+              }
+            }}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
