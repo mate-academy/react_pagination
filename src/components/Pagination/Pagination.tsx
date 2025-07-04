141e899215e683c05427fc8fc1a463dc25de9c6e
Jakub Podgrodzki <podgrodzkijakub@gmail.com>
2025-07-04 19:58:07 +0200

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,80 @@
-export const Pagination = () => {};
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  visibleItems: string[];
+  onPageChange: (number: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  visibleItems,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+
+        {Array.from({ length: totalPages }, (_, index) => {
+          const pageNumber = index + 1;
+
+          return (
+            <li
+              key={pageNumber}
+              className={`page-item ${pageNumber === currentPage ? 'active' : ''}`}
+            >
+              <a
+                data-cy="pageLink"
+                className="page-link"
+                href={`#${pageNumber}`}
+                onClick={() => onPageChange(pageNumber)}
+              >
+                {pageNumber}
+              </a>
+            </li>
+          );
+        })}
+
+        <li
+          className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === totalPages}
+            onClick={() =>
+              currentPage < totalPages && onPageChange(currentPage + 1)
+            }
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        {visibleItems.map(item => {
+          return (
+            <li data-cy="item" key={item}>
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
