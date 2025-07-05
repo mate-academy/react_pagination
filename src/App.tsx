0dcfd52bd7cc1e024b079087f52232ebabd9dbd8
ILLIA <bobicp01@gmail.com>
2025-07-05 09:50:30 +0200

    react-pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
 import { Pagination } from './components/Pagination';
