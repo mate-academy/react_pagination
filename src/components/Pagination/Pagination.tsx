bcc5529770ec50745973d5c9909597c39f14adcf
dminikulin <dminikulin2003@gmail.com>
2025-07-12 10:14:41 +0300

    Add Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,74 @@
-export const Pagination = () => {};
+import React from 'react';
+
+interface PaginationProps {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<PaginationProps> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+
+  if (totalPages === 0) {
+    return null;
+  }
+
+  const handlePageClick = (page: number, e: React.MouseEvent) => {
+    e.preventDefault();
+    if (page !== currentPage && page >= 1 && page <= totalPages) {
+      onPageChange(page);
+    }
+  };
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1 ? 'true' : 'false'}
+            onClick={e => handlePageClick(currentPage - 1, e)}
+          >
+            «
+          </a>
+        </li>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <li
+            key={i}
+            className={`page-item ${currentPage === i + 1 ? 'active' : ''}`}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href={'#' + (i + 1)}
+              onClick={e => handlePageClick(i + 1, e)}
+            >
+              {i + 1}
+            </a>
+          </li>
+        ))}
+        <li
+          className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled={currentPage === totalPages ? 'true' : 'false'}
+            onClick={e => handlePageClick(currentPage + 1, e)}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
