97f3d41a42d74370d80319046788e7424477715f
Nikita Zaluzhnyi <nikita@Nikitas-MacBook-Air.local>
2025-07-17 13:33:21 +0200

    first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { getNumbers } from './utils';
 
@@ -6,12 +6,30 @@ import { getNumbers } from './utils';
 const items = getNumbers(1, 42).map(n => `Item ${n}`);
 
 export const App: React.FC = () => {
+  const [itemsPerPage, setItemsPerPage] = useState<number>(5);
+  const [pageActive, setPageActive] = useState<number>(1);
+  const [numberOfPages, setNumberOfPages] = useState<number>(9);
+  const arrayNumberOfPage: number[] = [];
+  const end = Math.min(itemsPerPage * pageActive, items.length);
+  const start = itemsPerPage * (pageActive - 1) + 1;
+
+  for (let i = 1; i <= numberOfPages; i++) {
+    arrayNumberOfPage.push(Number(i));
+  }
+
+  function handlePerPageChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const newItemsPerPage = Number(event.target.value);
+
+    setItemsPerPage(newItemsPerPage);
+    setNumberOfPages(Math.ceil(42 / newItemsPerPage));
+  }
+
   return (
     <div className="container">
       <h1>Items with Pagination</h1>
 
       <p className="lead" data-cy="info">
-        Page 1 (items 1 - 5 of 42)
+        Page {pageActive} (items {start} - {end} of 42)
       </p>
 
       <div className="form-group row">
@@ -19,7 +37,10 @@ export const App: React.FC = () => {
           <select
             data-cy="perPageSelector"
             id="perPageSelector"
-            className="form-control">
+            className="form-control"
+            onChange={handlePerPageChange}
+            defaultValue={5}
+          >
             <option value="3">3</option>
             <option value="5">5</option>
             <option value="10">10</option>
@@ -34,76 +55,64 @@ export const App: React.FC = () => {
 
       {/* Move this markup to Pagination */}
       <ul className="pagination">
-        <li className="page-item disabled">
+        <li
+          className={`page-item ${pageActive === arrayNumberOfPage[0] ? 'disabled' : ''}`}
+        >
           <a
             data-cy="prevLink"
             className="page-link"
             href="#prev"
-            aria-disabled="true">
+            aria-disabled={pageActive === arrayNumberOfPage[0]}
+            onClick={() => {
+              if (pageActive > arrayNumberOfPage[0]) {
+                setPageActive(pageActive - 1);
+              }
+            }}
+          >
             «
           </a>
         </li>
-        <li className="page-item active">
-          <a data-cy="pageLink" className="page-link" href="#1">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#2">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#3">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#4">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#5">
-            5
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#6">
-            6
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#7">
-            7
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#8">
-            8
-          </a>
-        </li>
-        <li className="page-item">
-          <a data-cy="pageLink" className="page-link" href="#9">
-            9
-          </a>
-        </li>
-        <li className="page-item">
+        {arrayNumberOfPage.map(number => {
+          return (
+            <li
+              className={`page-item ${pageActive === number ? 'active' : ''}`}
+              onClick={() => setPageActive(number)}
+              key={number}
+            >
+              <a data-cy="pageLink" className="page-link" href={`#${number}`}>
+                {number}
+              </a>
+            </li>
+          );
+        })}
+        <li
+          className={`page-item ${pageActive === numberOfPages ? 'disabled' : ''}`}
+        >
           <a
             data-cy="nextLink"
-            className="page-link"
+            className={`page-link `}
             href="#next"
-            aria-disabled="false">
+            aria-disabled={pageActive === numberOfPages}
+            onClick={() => {
+              if (pageActive < arrayNumberOfPage.length) {
+                setPageActive(pageActive + 1);
+              }
+            }}
+          >
             »
           </a>
         </li>
       </ul>
       <ul>
-        <li data-cy="item">Item 1</li>
-        <li data-cy="item">Item 2</li>
-        <li data-cy="item">Item 3</li>
-        <li data-cy="item">Item 4</li>
-        <li data-cy="item">Item 5</li>
+        {items.map((item, index) => {
+          return index < end && index >= start - 1 ? (
+            <li data-cy="item" key={item}>
+              {item}
+            </li>
+          ) : (
+            <></>
+          );
+        })}
       </ul>
     </div>
   );
