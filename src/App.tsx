4cec631896b14837b5ec5c2715885f109034ecaa
Viktoriia <viktoriamyhailiak@gmail.com>
2025-07-22 13:14:23 +0300

    add a solution

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,10 @@ export const App: React.FC = () => {
             id="perPageSelector"
             className="form-control"
             onChange={handlePerPageChange}
+            value={perPage}
           >
             <option value="3">3</option>
-            <option value="5" selected>
-              5
-            </option>
+            <option value="5">5</option>
             <option value="10">10</option>
             <option value="20">20</option>
           </select>
