4051ed019279449206fd89f74cd5c66054021645
Vladislav-Korsun <vladosik385@gmail.com~>
2025-12-22 21:52:40 +0200

    add task fix

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 type PaginationProps = {
   total: number;
   perPage: number;
-  currentPage: number;
+  currentPage?: number;
   onPageChange: (page: number) => void;
 };
 
 export const Pagination: React.FC<PaginationProps> = ({
   total,
   perPage,
-  currentPage,
+  currentPage = 1,
   onPageChange,
 }) => {
   const pagesCount = Math.ceil(total / perPage);
