a125396b3a4cd6ebaef53836df524d178843d060
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2026-01-02 16:36:55 +0200

    Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,68 @@
-export const Pagination = () => {};
+import React from "react";
+
+type Props = {
+  total: number,
+  perPage: number,
+  currentPage: number,
+  onPageChange: (number: number) => void
+}
+
+export const Pagination: React.FC<Props> = ({total, perPage, currentPage, onPageChange}) => {
+    const totalPages = Math.ceil(total / perPage);
+    const pages = [];
+
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i)
+    };    
+    
+    return (
+        <ul className="pagination">
+            <li className={`page-item ${currentPage === pages[0] ? 'disabled' : ''}`}>
+              <a
+                data-cy="prevLink"
+                className="page-link"
+                href="#prev"
+                aria-disabled={currentPage === 1}
+                onClick={(event) => {
+                  event.preventDefault();
+                  if (currentPage > 1) {
+                    onPageChange(currentPage - 1)
+                  }
+                }}
+              >
+                «
+              </a>
+            </li>
+            {
+                pages.map((page) => (
+                    <li key={page} className={`page-item ${page === currentPage ? 'active' : ''}`}>
+                        <a onClick={
+                            (event) => {
+                              event.preventDefault()
+                              onPageChange(page)
+                            }
+                        } data-cy="pageLink" className="page-link" href={`#${page}`}>
+                            {page}
+                        </a>
+                    </li>
+                ))
+            }
+            <li className={`page-item ${currentPage === pages[pages.length - 1] ? 'disabled' : ''}`}>
+              <a
+                data-cy="nextLink"
+                className="page-link"
+                href="#next"
+                aria-disabled={currentPage === totalPages}
+                onClick={(event) => {
+                  event.preventDefault();
+                  if (currentPage < totalPages) {
+                    onPageChange(currentPage + 1)
+                  }
+                }}
+              >
+                »
+              </a>
+            </li>
+      </ul>
+    )
+};
