f3c63f256e62209357b278c565a0d1bade8d736c
Ruslan Kasinets <rulik.357@gmail.com>
2026-05-27 12:59:27 +0300

    Solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,95 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number;
+  itemsPerPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination = ({
+  total,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}: Props) => {
+  const pagesCount = Math.ceil(total / itemsPerPage);
+  const pages = getNumbers(1, pagesCount);
+
+  return (
+    <ul className="pagination">
+      <li
+        className={cn('page-item', {
+          disabled: currentPage === 1,
+        })}
+      >
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={e => {
+            e.preventDefault();
+
+            if (currentPage === 1) {
+              return;
+            }
+
+            onPageChange(currentPage - 1);
+          }}
+        >
+          «
+        </a>
+      </li>
+      {pages.map(page => (
+        <li
+          key={page}
+          className={cn('page-item', {
+            active: page === currentPage,
+          })}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={e => {
+              e.preventDefault();
+
+              if (page === currentPage) {
+                return;
+              }
+
+              onPageChange(page);
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li
+        className={cn('page-item', {
+          disabled: currentPage === pagesCount,
+        })}
+      >
+        <a
+          data-cy="nextLink"
+          className={cn('page-link')}
+          href="#next"
+          aria-disabled={currentPage === pagesCount}
+          onClick={e => {
+            e.preventDefault();
+
+            if (currentPage === pagesCount) {
+              return;
+            }
+
+            onPageChange(currentPage + 1);
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
