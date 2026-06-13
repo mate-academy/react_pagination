97bdf3242fb1a49ecc8e0e827e8c4bd6a72cc9ae
Vita Martsekhovska <marcehovskavita@gmail.com>
2026-06-13 16:56:44 +0300

    add task solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,83 @@
-export const Pagination = () => {};
+import React from 'react';
+import { getNumbers } from '../../utils';
+import cn from 'classnames';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pageGetNumbers = getNumbers(1, totalPages);
+
+  return (
+    <ul className="pagination">
+      <li className={cn('page-item', { disabled: currentPage === 1 })}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={event => {
+            event.preventDefault();
+            if (currentPage === 1) {
+              return;
+            }
+
+            onPageChange(currentPage - 1);
+          }}
+        >
+          «
+        </a>
+      </li>
+      {pageGetNumbers.map(page => (
+        <li
+          className={cn('page-item', { active: currentPage === page })}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={event => {
+              event.preventDefault();
+              if (page === currentPage) {
+                return;
+              }
+
+              onPageChange(page);
+            }}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li className={cn('page-item', { disabled: currentPage === totalPages })}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === totalPages}
+          onClick={event => {
+            event.preventDefault();
+            if (currentPage === totalPages) {
+              return;
+            }
+
+            onPageChange(currentPage + 1);
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
